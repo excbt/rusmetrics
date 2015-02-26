3676c056ae31886b786ebadd6d0240ec8d0067d5
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-02-26 16:26:10 +0300

    ContServiceType model + repository added
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

---
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/ContServiceType.java
@@ -0,0 +1,93 @@
+package ru.excbt.datafuse.nmk.data.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="cont_service_type")
+public class ContServiceType implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "keyname")
+	private String keyname;
+	
+	@Column(name = "cont_service_type_name")
+	private String name;
+	
+	@Column(name = "cont_service_type_comment")
+	private String comment;
+	
+	@Column(name = "ex_code")
+	private String exCode;
+	
+	@Version
+	private int version;	
+	
+	@Embedded
+	@JsonIgnore
+	private RowAudit rowAudit;
+
+	public String getKeyname() {
+		return keyname;
+	}
+
+	public void setKeyname(String keyname) {
+		this.keyname = keyname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getExCode() {
+		return exCode;
+	}
+
+	public void setExCode(String exCode) {
+		this.exCode = exCode;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public RowAudit getRowAudit() {
+		return rowAudit;
+	}
+
+	public void setRowAudit(RowAudit rowAudit) {
+		this.rowAudit = rowAudit;
+	}
+	
+	
+	
+}
