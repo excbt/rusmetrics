0e8c17138f2a5f56638f380bccd79c61993db1b5
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-02-27 15:15:09 +0300

    JSONB Support DeviceObject + DeviceObjectDataJson model

---
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/DeviceObjectDataJson.java
@@ -0,0 +1,105 @@
+package ru.excbt.datafuse.nmk.data.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import ru.excbt.datafuse.hibernate.types.StringJsonUserType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="device_object_data_json")
+@TypeDefs( {@TypeDef( name= "StringJsonObject", typeClass = StringJsonUserType.class)})
+public class DeviceObjectDataJson extends IdEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+ 
+	@Column(name="import_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date importDate;
+	
+	@Column(name="device_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date deviceDate;
+	
+	@Column(name="data_json")
+	@Type(type = "StringJsonObject")
+	@JsonIgnore // We don't want to pass this field to client
+	private String dataJson;
+	
+	@Column(name="device_data_type")
+	private String deviceDataType;
+	
+	@OneToOne (fetch = FetchType.LAZY) 
+	@JoinColumn(name = "device_object_id")
+	private DeviceObject deviceObject;
+	
+	@Column(name="time_detail_type")
+	private String timeDetailType;
+
+	public Date getImportDate() {
+		return importDate;
+	}
+
+	public void setImportDate(Date importDate) {
+		this.importDate = importDate;
+	}
+
+	public Date getDeviceDate() {
+		return deviceDate;
+	}
+
+	public void setDeviceDate(Date deviceDate) {
+		this.deviceDate = deviceDate;
+	}
+
+	public String getDataJson() {
+		return dataJson;
+	}
+
+	public void setDataJson(String dataJson) {
+		this.dataJson = dataJson;
+	}
+
+	public String getDeviceDataType() {
+		return deviceDataType;
+	}
+
+	public void setDeviceDataType(String deviceDataType) {
+		this.deviceDataType = deviceDataType;
+	}
+
+	public DeviceObject getDeviceObject() {
+		return deviceObject;
+	}
+
+	public void setDeviceObject(DeviceObject deviceObject) {
+		this.deviceObject = deviceObject;
+	}
+
+	public String getTimeDetailType() {
+		return timeDetailType;
+	}
+
+	public void setTimeDetailType(String timeDetailType) {
+		this.timeDetailType = timeDetailType;
+	}
+	
+}
