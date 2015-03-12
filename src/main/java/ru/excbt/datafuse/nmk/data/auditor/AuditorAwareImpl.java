340368ceb73efaa0290d05d174b5097cd78d1cf8
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-03-12 17:36:38 +0300

    CurrentUserService upgrade. Merge with AuditUserService

--- src/main/java/ru/excbt/datafuse/nmk/data/auditor/AuditorAwareImpl.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/auditor/AuditorAwareImpl.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 
 import ru.excbt.datafuse.nmk.data.model.AuditUser;
-import ru.excbt.datafuse.nmk.data.service.AuditUserService;
+import ru.excbt.datafuse.nmk.data.service.CurrentUserService;
 
 public class AuditorAwareImpl implements AuditorAware<AuditUser> {
 
 	@Autowired
-	private AuditUserService currentUserService;
+	private CurrentUserService currentUserService;
 	
 	@Override
 	public AuditUser getCurrentAuditor() {
-		return currentUserService.getCurrentUser();
+		return currentUserService.getCurrentAuditUser();
 	}
 
 }
