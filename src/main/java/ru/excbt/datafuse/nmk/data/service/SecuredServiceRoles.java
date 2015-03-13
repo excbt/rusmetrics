554199ddfe4cfd6c8340ac018169d07c4595370d
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-03-13 11:22:29 +0300

    SecuredServiceRoles interface added

---
+++ src/main/java/ru/excbt/datafuse/nmk/data/service/SecuredServiceRoles.java
@@ -0,0 +1,6 @@
+package ru.excbt.datafuse.nmk.data.service;
+
+public interface SecuredServiceRoles {
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String SUBSCR_ROLE_ADMIN = "SUBSCR_ROLE_ADMIN";
+}
