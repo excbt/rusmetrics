7f6f75f6c4ec201ad1ec60e3c189381c0d0c4c67
a.artamonov <a.artamonov@excbt.ru>
2015-04-06 13:09:31 +0400

    Remove no relevated links.
    Signed-off-by: a.artamonov <a.artamonov@excbt.ru>

---
+++ src/main/webapp/app/scripts/controllers/data-range-settings.js
@@ -0,0 +1,71 @@
+var app = angular.module('portalNMK');
+app
+    .controller('DataRangeSettings', function($scope, $rootScope){
+                    $scope.navPlayerDates = {
+                            startDate : moment().startOf('day'),
+                            endDate : moment().endOf('day'),
+                        };
+
+                    $scope.queryDateOptsRu = {
+                                locale : {
+                                    applyClass : 'btn-green',
+                                    applyLabel : "Применить",
+                                    fromLabel : "с",
+                                    toLabel : "по",
+                                    cancelLabel : 'Отмена',
+                                    customRangeLabel : 'Период',
+                                    daysOfWeek : [ 'Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб' ],
+                                    firstDay : 1,
+                                    monthNames : [ 'Январь', 'Февраль', 'Март', 'Апрель',
+                                            'Май', 'Июнь', 'Июль', 'Август', 'Сентабрь',
+                                            'Октябрь', 'Ноябрь', 'Декабрь' ]
+                                },
+                                ranges : {
+                                    'Текущий день' : [ moment().startOf('day'),
+                                            moment().endOf('day') ],
+                                    'Посл 7 дней' : [
+                                            moment().subtract(6, 'days').startOf('day'),
+                                            moment().endOf('day') ],
+                                    'Посл 30 дней' : [
+                                            moment().subtract(29, 'days').startOf('day'),
+                                            moment().endOf('day') ]
+                                },
+                                startDate : moment().startOf('day'),
+                                endDate : moment().endOf('day'),
+
+                                format : 'DD-MM-YYYY'
+                            };
+
+                        $scope.queryDateOptsEn = {
+                                locale : {
+                                    applyClass : 'btn-green',
+                                },
+                                ranges : {
+                                    'Today' : [ moment().startOf('day'),
+                                            moment().endOf('day') ],
+                                    'Last 7 days' : [
+                                            moment().subtract(6, 'days')
+                                                    .startOf('day'),
+                                            moment().endOf('day') ],
+                                    'Last 30 days' : [
+                                            moment().subtract(29, 'days')
+                                                    .startOf('day'),
+                                            moment().endOf('day') ]
+                                },
+                                startDate : moment().startOf('day'),
+                                endDate : moment().endOf('day'),
+
+                                format : 'DD-MM-YYYY'
+                        };
+    
+                        $scope.$watch('navPlayerDates', function (newDates) {
+console.log("newDates[]= "+newDates);
+for (var k in newDates){                            
+    console.log("newDates["+k+"]= "+newDates[k]);
+};
+                            $rootScope.reportStart = moment(newDates.startDate).format('YYYY-MM-DD');
+                            $rootScope.reportEnd = moment(newDates.endDate).format('YYYY-MM-DD');
+console.log("This change");                            
+                            //  $scope.getReport(newDates);                
+                        }, false);
+});
