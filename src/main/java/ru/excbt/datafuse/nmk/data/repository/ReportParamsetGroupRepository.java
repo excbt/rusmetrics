84c2473e1f177775dfac8b285aa844b31df7e660
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-04-15 10:31:49 +0300

    ReportTemplate reportType upgrade
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/data/repository/ReportParamsetGroupRepository.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/repository/ReportParamsetGroupRepository.java
@@ -6,4 +6,9 @@ import ru.excbt.datafuse.nmk.data.model.ReportParamsetGroup;
 
 public interface ReportParamsetGroupRepository extends CrudRepository<ReportParamsetGroup, Long> {
 
+//	@Query("SELECT rp FROM ReportParamset rp LEFT JOIN FETCH rp.reportTemplate rt "
+//			+ "WHERE rp.subscriber is null AND rt.reportTypeKey = :reportType AND "
+//			+ "rp._active = :isActive "
+//			+ "ORDER BY rp.activeStartDate, rp.name")
+//	public List<ContObject> selectContObjectGroup(@Param("reportParamsetId") long reportParamsetId);
 }
