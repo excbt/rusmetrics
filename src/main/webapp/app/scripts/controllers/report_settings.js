897f388543103591d2264148d10d0f7fec799406
a.artamonov <a.artamonov@excbt.ru>
2015-04-15 12:26:23 +0400

    Connect rest to report templates.
    Signed-off-by: a.artamonov <a.artamonov@excbt.ru>

--- src/main/webapp/app/scripts/controllers/report_settings.js
+++ src/main/webapp/app/scripts/controllers/report_settings.js
@@ -1,43 +1,119 @@
 'use strict';
 var app = angular.module('portalNMK');
 
-app.controller('ReportSettingsCtrl',['$scope',function($scope){
+app.controller('ReportSettingsCtrl',['$scope', 'crudGridDataFactory', 'notificationFactory', function($scope,crudGridDataFactory, notificationFactory){
     
     $scope.active_tab_active_templates = true;
     
     $scope.columns = [
-        {"name":"reportType","header":"Тип отчета", "class":"col-md-11"}
+        {"name":"reportTypeName","header":"Тип отчета", "class":"col-md-11"}
 //        ,{"name":"templatesCount", "header":"Кол-во шаблонов", "class":"col-md-1"}
     ];
     
+    $scope.commerce = {};
+    $scope.cons = {};
+    $scope.event = {};
+    
+    $scope.crudTableName = "../api/reportTemplate"; 
+    
+//    $scope.reportTypes = [];
+//    $scope.getReportTypes = function(){
+//        var table = "../api/reportSettings/reportType";
+//        crudGridDataFactory(table).query(function(data){
+//            $scope.reportTypes = data;
+//        });
+//    };
+//    $scope.getReportTypes();
+//    
+//    $scope.reportPeriods = [];
+//    $scope.getReportPeriods = function(){
+//        var table = "../api/reportSettings/reportPeriod";
+//        grudGridDataFactory(table).query(function(data){
+//            $scope.reportPeriods = data;
+//        });
+//    };
+    
     $scope.objects = [
         {
-            "reportType":"Коммерческий"
-            ,"templatesCount":" 2"
+            "reportTypeName":"Коммерческий"
+            ,"templatesCount": 0
+            ,"templates": []
         }
         ,        {
-            "reportType":"Сводный"
-            ,"templatesCount":" 0"
+            "reportTypeName":"Сводный"
+            ,"templatesCount":0
+            ,"templates": []
         }
         ,        {
-            "reportType":"События"
-            ,"templatesCount":" 1"
+            "reportTypeName":"События"
+            ,"templatesCount": 0
+            ,"templates": []
         }
     ];
     
     $scope.oldColumns = [
-        {"name":"reportTemplateName", "header":"Название шаблона", "class":"col-md-5"}
-        ,{"name":"reportTemplateDate", "header":"Дата создания", "class":"col-md-2"}
+        {"name":"name", "header":"Название шаблона", "class":"col-md-5"}
+        ,{"name":"activeStartDate", "header":"Действует с", "class":"col-md-2"}
     ];
     
-    $scope.oldObjects = [
-        {"reportTemplateName":"Дефолт"
-         ,"reportTemplateDate": "01.04.2015"
-        }
-        ,        {"reportTemplateName":"Template 1"
-         ,"reportTemplateDate": "02.04.2015"
-        }
-    ];
+    
+    var successCallback = function (e) {
+        notificationFactory.success();
+        $('#deleteObjectModal').modal('hide');
+        $scope.currentObject={};
+    };
+
+
+    var errorCallback = function (e) {
+        notificationFactory.error(e.data.ExceptionMessage);
+    };
+    
+    
+      
+    $scope.getCommerceTemplates = function (table) {
+        crudGridDataFactory(table).query(function (data) {
+            $scope.commerce.templates = data;
+            $scope.objects[0].templatesCount = $scope.commerce.templates.length;
+            $scope.objects[0].templates = $scope.commerce.templates;
+        });
+    };
+    $scope.getConsTemplates = function (table) {
+        crudGridDataFactory(table).query(function (data) {
+            $scope.cons.templates = data; 
+            $scope.objects[1].templatesCount = $scope.cons.templates.length;
+            $scope.objects[1].templates = $scope.cons.templates;
+        });
+    };
+    $scope.getEventTemplates = function (table) {
+        crudGridDataFactory(table).query(function (data) {
+            $scope.event.templates = data;
+            $scope.objects[2].templatesCount = $scope.event.templates.length;
+            $scope.objects[2].templates = $scope.event.templates;
+        });
+    };
+ //get templates   
+    $scope.getActive = function(){
+        $scope.getCommerceTemplates($scope.crudTableName+"/commerce");
+        $scope.getConsTemplates($scope.crudTableName+"/cons");
+        $scope.getEventTemplates($scope.crudTableName+"/event");
+    };
+    
+    $scope.getActive();
+    
+    $scope.getArchive = function(){
+        $scope.getCommerceTemplates($scope.crudTableName+"/archive/commerce");
+        $scope.getConsTemplates($scope.crudTableName+"/archive/cons");
+        $scope.getEventTemplates($scope.crudTableName+"/archive/event");
+    };
+    
+//    $scope.oldObjects = [
+//        {"reportTemplateName":"Дефолт"
+//         ,"reportTemplateDate": "01.04.2015"
+//        }
+//        ,        {"reportTemplateName":"Template 1"
+//         ,"reportTemplateDate": "02.04.2015"
+//        }
+//    ];
     
     $scope.toogleShowGroupDetails = function(curObject){//switch option: current goup details
 //                   for(var i=0;i<$scope.objects.length;i++){
@@ -48,6 +124,51 @@ app.controller('ReportSettingsCtrl',['$scope',function($scope){
                     curObject.showGroupDetails = !curObject.showGroupDetails;
                     
                   //  $scope.selectedItem(curObject);
+    };
+    
+    $scope.currentObject = {};
+    $scope.selectedItem = function(item){
+//console.log("In selected item.");        
+        var curObject = angular.copy(item);
+		$scope.currentObject = curObject;
+//console.log("Selected item = ");        
+//for(var k in $scope.currentObject){
+//console.log("$scope.currentObject["+k+"]="+$scope.currentObject[k]);    
+//}        
+    };
+    
+    $scope.updateTemplate = function(object){
+        var table = "";
+        var id = object.id;
+        object.id = null;
+        if (!object._active){
+            table = $scope.crudTableName+"/createByTemplate/"    
+            crudGridDataFactory(table).save({srcReportTemplateId: id},object, successCallback, errorCallback);
+            return;
+        };
+        var reportType = object.reportTypeKey;
+        
+        switch (reportType){
+            case "COMMERCE_REPORT":  table=$scope.crudTableName+"/commerce/"; break;   
+            case "CONS_REPORT":  table=$scope.crudTableName+"/cons/"; break;   
+            case "EVENT_REPORT":  table=$scope.crudTableName+"/events/"; break;       
+        };
+        crudGridDataFactory(table).update({reportTemplateId: object.id}, object, successCallback, errorCallback);
+    };
+    
+    $scope.moveToArchive = function(object){
+        var table = $scope.crudTableName+"/archive/move"    
+        crudGridDataFactory(table).update({reportTemplateId: object.id}, object, successCallback, errorCallback);
+    };
+    
+//    $scope.createTemplate = function(object){
+//        var table = $scope.crudTableName+"/archive/move"    
+//        crudGridDataFactory(table).save(object, successCallback, errorCallback);
+//    };
+    
+    $scope.createByTemplate =  function(id, object){
+        var table = $scope.crudTableName+"/createByTemplate/"    
+        crudGridDataFactory(table).save({srcReportTemplateId: id},object, successCallback, errorCallback);
     };
     
     
@@ -64,41 +185,41 @@ app.controller('ReportSettingsCtrl',['$scope',function($scope){
         }
         ,{"name":"Колонка3"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка4"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка5"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка6"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка7"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка8"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка9"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка12"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка13"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка22"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка23"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка32"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка33"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка42"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка43"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка52"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка53"
         }
-        ,{"name":"Колонка2"
+        ,{"name":"Колонка62"
         }
-        ,{"name":"Колонка3"
+        ,{"name":"Колонка63"
         }
     ];    
     $scope.system1.defineColumns = [
@@ -116,73 +237,102 @@ app.controller('ReportSettingsCtrl',['$scope',function($scope){
     $scope.system2.defineColumns = [
     ];
 
-    $scope.addColumns= function(defaultColumns){
-        var result=[];
+    $scope.addColumns= function(defaultColumns, defineColumns){
+        var result = defineColumns;
         var colSelected = 0;
-       // var tmp = $scope.defaultColumns.map(function(el){if (el.selected) {el.selectedforremove=false;return el}});
+      
         for (var i =0; i<defaultColumns.length; i++)
         {
-
-            if (defaultColumns[i].selected){                           
-                result[colSelected] = defaultColumns[i];
+            if (defaultColumns[i].selected){  
+                defaultColumns[i].class="";
+                defaultColumns[i].selected = false;
+                if (typeof defineColumns != 'undefined'){
+                    var flagElementAlreadyAdded = false;
+                    for (var j=0; j<=defineColumns.length; j++){
+                        if (typeof defineColumns[j] != 'undefined' && (defineColumns[j].name == defaultColumns[i].name)){
+                            flagElementAlreadyAdded = true;
+                        };
+                    }
+                
+                    if (flagElementAlreadyAdded){continue;};
+                }
+                     
+                var elem = angular.copy(defaultColumns[i]);
+                elem.selected = false;
+                elem.class = "";
+                result.push(elem);
                 colSelected=colSelected+1;                
             };
-
         }
              
         return result;
     };
     
-    $scope.removeColumns= function(defaultColumns, defineColumns){
-       //  var tmp = $scope.defineColumns.map(function(el){if (!el.selectedforremove) {return el}});
-        var tmp= [];
-        var colSelected = 0;
+    $scope.removeColumns= function(defineColumns){
+        var result= [];
+//        var colSelected = 0;
 
         for (var i =0; i<defineColumns.length; i++)
         {
-            if (!defineColumns[i].selectedforremove){
-                tmp[colSelected] = defineColumns[i];
-                colSelected+=1;
+            if (!defineColumns[i].selected){
+                defineColumns[i].selected = false;
+                result.push(defineColumns[i]);
+//                result[colSelected].selected = false;
+//                colSelected+=1;
             };
 
         }
-        defineColumns = tmp;
+        return result;
     };
 
-    $scope.moveColumnsUp= function(){
-        var tmp= [];
-        var colSelected = 0;
+    $scope.moveColumnsUp= function(defineColumns){
+        var tmp={};
+  //      var colSelected = 0;
 
-        for (var i =0; i<$scope.defineColumns.length; i++)
+        for (var i =1; i<defineColumns.length; i++)
         {
-            if (!$scope.defineColumns[i].selectedforremove){
-                tmp[colSelected] = $scope.defineColumns[i];
-                colSelected+=1;
+            if (defineColumns[i].selected){
+                tmp = defineColumns[i-1];
+                defineColumns[i-1] = defineColumns[i];
+                defineColumns[i] = tmp;
+//                tmp[colSelected] = defineColumns[i];
+//                colSelected+=1;
             };
 
         }
-        $scope.defineColumns = tmp;
+        return defineColumns;
     };
 
-    $scope.moveColumnsDown= function(){
-        var tmp= [];
-        var colSelected = 0;
+    $scope.moveColumnsDown= function(defineColumns){
+                var tmp={};
+  //      var colSelected = 0;
 
-        for (var i =0; i<$scope.defineColumns.length; i++)
+        for (var i =defineColumns.length-2; i>=0; i--)
         {
-            if (!$scope.defineColumns[i].selectedforremove){
-                tmp[colSelected] = $scope.defineColumns[i];
-                colSelected+=1;
+            if (defineColumns[i].selected){
+                tmp = defineColumns[i+1];
+                defineColumns[i+1] = defineColumns[i];
+                defineColumns[i] = tmp;
+//                tmp[colSelected] = defineColumns[i];
+//                colSelected+=1;
             };
 
         }
-        $scope.defineColumns = tmp;
+        return defineColumns;
     };
     
     $scope.changeSystemPosition = function(){
        var tmp = $scope.systems[0]; 
         $scope.systems[0] = $scope.systems[1]; 
         $scope.systems[1] = tmp; 
-    };            
+    }; 
+    
+    $scope.selectColumn = function(item){
+        item.selected = !item.selected;
+        item.class= item.selected?"active":"";
+    };
+    
+    
+
     
 }]);
