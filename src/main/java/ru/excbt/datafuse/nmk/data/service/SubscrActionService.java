6f63ec5876e9a9b7e6d790cfabdcc920ff47b65f
s.kuzovoy <s.kuzovoy@excbt.ru>
2015-04-24 16:13:25 +0300

    added findActionUserOne, UpdateOneUser in SubscrActionUser && SubscrActionController

--- src/main/java/ru/excbt/datafuse/nmk/data/service/SubscrActionService.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/service/SubscrActionService.java
@@ -3,6 +3,7 @@ package ru.excbt.datafuse.nmk.data.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,10 +11,14 @@ import ru.excbt.datafuse.nmk.data.model.SubscrActionGroup;
 import ru.excbt.datafuse.nmk.data.model.SubscrActionUser;
 import ru.excbt.datafuse.nmk.data.repository.SubscrActionGroupRepository;
 import ru.excbt.datafuse.nmk.data.repository.SubscrActionUserRepository;
+import ru.excbt.datafuse.nmk.security.SecuredRoles;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 
 @Service
 @Transactional
-public class SubscrActionService {
+public class SubscrActionService implements SecuredRoles {
 
 	@Autowired
 	private SubscrActionGroupRepository subscrActionGroupRepository;
@@ -30,6 +35,17 @@ public class SubscrActionService {
 	public List<SubscrActionGroup> findActionGroup(long subscriberId) {
 		return subscrActionGroupRepository.findBySubscriberId(subscriberId);
 	}
+
+	/**
+	 * 
+	 * @param subscriberId
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public SubscrActionGroup findActionGroupOne(long id) {
+		return subscrActionGroupRepository.findOne(id);
+	}
+	
 	
 	/**
 	 * 
@@ -41,4 +57,38 @@ public class SubscrActionService {
 		return subscrActionUserRepository.findBySubscriberId(subscriberId);
 	}
 	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public SubscrActionUser findActionUserOne(long id) {
+		return subscrActionUserRepository.findOne(id);
+	}
+	
+	
+	/**
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	@Secured ({ROLE_ADMIN, SUBSCR_ROLE_ADMIN})
+	public SubscrActionGroup updateOneGroup(SubscrActionGroup entity) {
+		checkArgument(!entity.isNew());
+		checkNotNull(entity.getSubscriber());
+		return subscrActionGroupRepository.save(entity);
+	}
+
+	/**
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	@Secured ({ROLE_ADMIN, SUBSCR_ROLE_ADMIN})
+	public SubscrActionUser updateOneUser(SubscrActionUser entity) {
+		checkArgument(!entity.isNew());
+		checkNotNull(entity.getSubscriber());
+		return subscrActionUserRepository.save(entity);
+	}
 }
--- src/main/java/ru/excbt/datafuse/nmk/web/api/SubscrActionController.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/api/SubscrActionController.java
@@ -1,15 +1,28 @@
 package ru.excbt.datafuse.nmk.web.api;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.List;
 
+import javax.persistence.PersistenceException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ru.excbt.datafuse.nmk.data.model.SubscrActionGroup;
 import ru.excbt.datafuse.nmk.data.model.SubscrActionUser;
+import ru.excbt.datafuse.nmk.data.model.UDirectory;
 import ru.excbt.datafuse.nmk.data.service.SubscrActionService;
 import ru.excbt.datafuse.nmk.data.service.support.CurrentSubscriberService;
 
@@ -17,6 +30,9 @@ import ru.excbt.datafuse.nmk.data.service.support.CurrentSubscriberService;
 @RequestMapping("/api/subscr/subscrAction")
 public class SubscrActionController extends WebApiController {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(	SubscrActionController.class);
+	
 	@Autowired
 	private SubscrActionService subscrActionService;
 
@@ -36,6 +52,45 @@ public class SubscrActionController extends WebApiController {
 	
 	/**
 	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/groups/{id}", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8)
+	public ResponseEntity<?> getOneGroup(@PathVariable("id") long id) {
+		return ResponseEntity.ok(subscrActionService.findActionGroupOne(id));
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param entity
+	 * @return
+	 */
+	@RequestMapping(value = "/groups/{id}", method = RequestMethod.PUT, produces = APPLICATION_JSON_UTF8)
+	public ResponseEntity<?> updateOneGroup(@PathVariable("id") long id,
+			@RequestBody SubscrActionGroup entity) {
+
+		checkNotNull(entity);
+		checkNotNull(entity.getId());
+		checkArgument(entity.getId().longValue() == id);
+
+		entity.setSubscriber(currentSubscriberService.getSubscriber());
+		
+		try {
+			subscrActionService.updateOneGroup(entity);
+		} catch (AccessDeniedException e) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		} catch (TransactionSystemException | PersistenceException e) {
+			logger.error("Error during save entity SubscrActionGroup (id={}): {}", id,
+					e);
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.build();
+		}
+		return ResponseEntity.accepted().build();
+	}	
+	
+	/**
+	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8)
@@ -45,4 +100,43 @@ public class SubscrActionController extends WebApiController {
 		return ResponseEntity.ok(resultList);
 	}
 
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8)
+	public ResponseEntity<?> getOneUser(@PathVariable("id") long id) {
+		return ResponseEntity.ok(subscrActionService.findActionUserOne(id));
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @param entity
+	 * @return
+	 */
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.PUT, produces = APPLICATION_JSON_UTF8)
+	public ResponseEntity<?> updateOneUser(@PathVariable("id") long id,
+			@RequestBody SubscrActionUser entity) {
+
+		checkNotNull(entity);
+		checkNotNull(entity.getId());
+		checkArgument(entity.getId().longValue() == id);
+
+		entity.setSubscriber(currentSubscriberService.getSubscriber());
+		
+		try {
+			subscrActionService.updateOneUser(entity);
+		} catch (AccessDeniedException e) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		} catch (TransactionSystemException | PersistenceException e) {
+			logger.error("Error during save entity SubscrActionUser (id={}): {}", id,
+					e);
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.build();
+		}
+		return ResponseEntity.accepted().build();
+	}	
+	
 }
