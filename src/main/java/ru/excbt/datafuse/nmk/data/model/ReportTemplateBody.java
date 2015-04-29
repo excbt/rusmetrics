b891870ee3525a1c535d22e922dd6a2286bd2ba5
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-04-29 12:20:21 +0300

    ReportTemplateBody Audited upgrade
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/data/model/ReportTemplateBody.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/ReportTemplateBody.java
@@ -2,19 +2,32 @@ package ru.excbt.datafuse.nmk.data.model;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Version;
 
 import org.hibernate.annotations.DynamicUpdate;
+import org.joda.time.DateTime;
+import org.springframework.data.domain.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "report_template_body")
 @DynamicUpdate
-public class ReportTemplateBody implements Serializable {
+@EntityListeners({ AuditingEntityListener.class })
+public class ReportTemplateBody implements Serializable,
+		Auditable<AuditUser, Long> {
 
 	/**
 	 * 
@@ -39,7 +52,27 @@ public class ReportTemplateBody implements Serializable {
 
 	@Column(name = "report_template_body_compiled_filename")
 	private String bodyCompiledFilename;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "created_by", updatable = false)
+	@JsonIgnore
+	private AuditUser createdBy;
+
+	@Column(name = "created_date", insertable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonIgnore
+	private Date createdDate;
+
+	@ManyToOne
+	@JoinColumn(name = "last_modified_by")
+	@JsonIgnore
+	private AuditUser lastModifiedBy;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "last_modified_date")
+	@JsonIgnore
+	private Date lastModifiedDate;
+
 	public Long getReportTemplateId() {
 		return reportTemplateId;
 	}
@@ -49,16 +82,14 @@ public class ReportTemplateBody implements Serializable {
 	}
 
 	public byte[] getBody() {
-		return body == null ? null : Arrays.copyOf(
-				body, body.length);
+		return body == null ? null : Arrays.copyOf(body, body.length);
 	}
 
 	public void setBody(byte[] body) {
 		if (body == null) {
 			this.body = null;
 		} else {
-			this.body = Arrays.copyOf(body,
-					body.length);
+			this.body = Arrays.copyOf(body, body.length);
 		}
 
 	}
@@ -72,17 +103,16 @@ public class ReportTemplateBody implements Serializable {
 	}
 
 	public byte[] getBodyCompiled() {
-		return bodyCompiled == null ? null : Arrays.copyOf(
-				bodyCompiled, bodyCompiled.length);
+		return bodyCompiled == null ? null : Arrays.copyOf(bodyCompiled,
+				bodyCompiled.length);
 	}
 
 	public void setBodyCompiled(byte[] bodyCompiled) {
 		if (bodyCompiled == null) {
 			this.bodyCompiled = null;
 		} else {
-			this.bodyCompiled = Arrays.copyOf(
-					bodyCompiled,
-					bodyCompiled.length);
+			this.bodyCompiled = Arrays
+					.copyOf(bodyCompiled, bodyCompiled.length);
 		}
 	}
 
@@ -98,8 +128,58 @@ public class ReportTemplateBody implements Serializable {
 		return bodyCompiledFilename;
 	}
 
-	public void setBodyCompiledFilename(
-			String bodyCompiledFilename) {
+	public void setBodyCompiledFilename(String bodyCompiledFilename) {
 		this.bodyCompiledFilename = bodyCompiledFilename;
+	}
+
+	@Override
+	public Long getId() {
+		return reportTemplateId;
+	}
+
+	@Override
+	public boolean isNew() {
+		return reportTemplateId == null;
+	}
+
+	@Override
+	public AuditUser getCreatedBy() {
+		return createdBy;
+	}
+
+	@Override
+	public void setCreatedBy(AuditUser createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	@Override
+	public DateTime getCreatedDate() {
+		return new DateTime(createdDate);
+	}
+
+	@Override
+	public void setCreatedDate(DateTime creationDate) {
+		this.createdDate = creationDate != null ? creationDate.toDate() : null;
+	}
+
+	@Override
+	public AuditUser getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	@Override
+	public void setLastModifiedBy(AuditUser lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	@Override
+	public DateTime getLastModifiedDate() {
+		return new DateTime(lastModifiedDate);
+	}
+
+	@Override
+	public void setLastModifiedDate(DateTime lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate != null ? lastModifiedDate
+				.toDate() : null;
 	}
 }
