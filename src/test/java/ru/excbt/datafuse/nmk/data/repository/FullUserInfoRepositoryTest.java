6e67b5c39d5315278e5cb50333188ce5628920d2
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-04-29 11:18:16 +0300

    FullUserInfo Model fix + Repository update + Test added
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

---
+++ src/test/java/ru/excbt/datafuse/nmk/data/repository/FullUserInfoRepositoryTest.java
@@ -0,0 +1,36 @@
+package ru.excbt.datafuse.nmk.data.repository;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ru.excbt.datafuse.nmk.data.JpaSupportTest;
+import ru.excbt.datafuse.nmk.data.model.FullUserInfo;
+
+public class FullUserInfoRepositoryTest extends JpaSupportTest {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(FullUserInfoRepositoryTest.class);
+
+	@Autowired
+	private FullUserInfoRepository fullUserInfoRepository;
+
+	@Test
+	public void testReporsitory() {
+		List<FullUserInfo> lst = fullUserInfoRepository
+				.findByUserName("test-argon19");
+		assertTrue(lst.size() > 0);
+
+		FullUserInfo result = new FullUserInfo(lst.get(0));
+
+		assertNotNull(result.getUserName());
+		logger.info("UserName: {}", result.getUserName());		
+		assertNotNull(result.getSubscriber());
+	}
+}
