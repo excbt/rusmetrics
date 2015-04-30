02231f4d967f16a67ebae6dd291ee1cde44039d5
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-04-30 12:33:26 +0300

    TariffPlanService update
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/test/java/ru/excbt/datafuse/nmk/data/service/TariffPlanServiceTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/data/service/TariffPlanServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,29 +33,37 @@ public class TariffPlanServiceTest extends JpaSupportTest {
 	}
 
 	@Test
-	@Ignore
+	//@Ignore
 	public void testUpdateTarifPlan() {
 
-		
-		
 		List<TariffPlan> tpList = tariffPlanService.selectTariffPlanList();
-		
+
 		assertTrue(tpList.size() > 0);
-		
+
 		TariffPlan tp = tpList.get(0);
-		
+
 		List<ContObject> contObjects = tariffPlanService
 				.selectTariffPlanAvailableContObjects(tp.getId(),
 						currentSubscriberService.getSubscriberId());
 
 		assertTrue(contObjects.size() > 0);
-		
+
 		tp.getContObjects().add(contObjects.get(0));
-		
+
 		TariffPlan result = tariffPlanService.updateOne(tp);
 		assertNotNull(result);
 		// tariffPlanService.deleteDefaultTariffPlan(TEST_RSO_ID );
-		//tariffPlanService.initDefaultTariffPlan(TEST_RSO_ID);
+		// tariffPlanService.initDefaultTariffPlan(TEST_RSO_ID);
+	}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testUpdateTarifPlanIllgal() {
+
+		List<TariffPlan> tpList = tariffPlanService.selectTariffPlanList();
+		assertTrue(tpList.size() > 0);
+		TariffPlan tp = tpList.get(0);
+		tp.setEndDate(DateTime.now().minusYears(10).toDate());
+		tariffPlanService.updateOne(tp);
 	}
 
 }
