454b4985d4b6fac62c490a7d41bf710c28a601e3
Osutin Artyom <osutin@excbt.ru>
2015-05-21 12:49:24 +0300

    contacts on angular-js

--- src/main/webapp/app/scripts/controllers/delivery.js
+++ src/main/webapp/app/scripts/controllers/delivery.js
@@ -1,12 +1,34 @@
 var app = angular.module('portalNMK');
 app.controller(
 		'DlvrCtrl',
-		function($scope, $http){
+		function($scope, $http, notificationFactory){
+			/************************************
+			 * Определяем глобальные переменные *
+			 ************************************/
+			// Массивы с данными
+			// Список рассылок
+			$scope.rep_shdls = [];
+			// Переменные с url'ами запросов
+			$scope.url_rep_shdls = "../api/reportShedule";
+			
+			/****************************
+			 * Функции для работы с API *
+			 ****************************/
+			// Получение списка рассылок
+			$scope.getReportShedules = function(){
+				$http.get($scope.url_rep_shdls)
+					.success(function(data){
+						$scope.rep_shdls = data;
+					})
+					.error(function(e){
+						notificationFactory.errorInfo(e.statusText,e.description);
+					});
+			}
+			
 			 $scope.dlvrSave = function(){
 				var slct_rep_type = document.getElementById('slct_rep_tmpl_var');
 				var tmpl_id = slct_rep_type[slct_rep_type.selectedIndex].id;
 				var paramset_id = slct_rep_type.value;
-//				alert(tmpl_id);
 				// Подготовка данных для сохранения
 				var dlvr_data = new Object ();
 				dlvr_data['name'] = document.getElementById('inp_dlvr_name').value;
@@ -48,14 +70,6 @@ app.controller(
 								//Добавляем новый элемент в массив ...
 								addDelivery(data);
 								// ... и в таблицу
-							/*	-- вариант с наименьшим трафиком. Надо доделать.
-                            var tbl_dlvr = document.getElementById('table_deliveries');
-								newRow = tbl_dlvr.insertRow();
-								newRow.id = "dlvr_id_" + data['id'];
-								newRow.insertCell().innerHTML = "<i title='Удалить' class='btn btn-default btn-xs glyphicon glyphicon-trash' onclick = delDlvrTd(" + data['id'] + ")></i> <i title='Изменить' class='btn btn-default btn-xs glyphicon glyphicon-edit' onclick=dlvrEditWindow(" + data['id'] + ") data-target='#div_edit_delivery' data-toggle='modal'></i>";
-								newRow.insertCell().innerHTML = data['name'];
-								newRow.insertCell().innerHTML = data['description'];*/
-
                                 document.getElementById('table_deliveries').innerHTML = '<tr id="tr_dlv_header" class="info">\
                                     <th class="col-md-1">#</th>\
                                     <th class="col-md-2">Название</th>\
@@ -73,7 +87,6 @@ app.controller(
 							'../api/reportShedule/' + dlvr_data['id'] + '?reportTemplateId=' + tmpl_id + '&reportParamsetId=' + paramset_id,
 							dlvr_data)
 							.success(function(data){
-//								updateDeliveryDetails(data); -- вариант с наименьшим трафиком. Надо доделать.
                                 document.getElementById('table_deliveries').innerHTML = '<tr id="tr_dlv_header" class="info">\
                                     <th class="col-md-1">#</th>\
                                     <th class="col-md-2">Название</th>\
@@ -84,5 +97,9 @@ app.controller(
 							});
 				}
 			}
+			/*********************
+			 * Запускаем функции *
+			 *********************/
+			$scope.getReportShedules();
 		}	
 );
