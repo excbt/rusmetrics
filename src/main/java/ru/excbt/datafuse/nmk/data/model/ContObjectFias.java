a4d484b709fefec4ec5311b215baf5579728001e
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-07-20 16:05:17 +0300

    ContObjectFias model + repository added
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

---
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/ContObjectFias.java
@@ -0,0 +1,85 @@
+package ru.excbt.datafuse.nmk.data.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Type;
+
+import ru.excbt.datafuse.nmk.data.domain.AbstractAuditableModel;
+
+@Entity
+@Table(name = "cont_object_fias")
+@DynamicUpdate
+public class ContObjectFias extends AbstractAuditableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4834456607858555535L;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "cont_object_id", insertable = false, updatable = false)
+	private ContObject contObject;
+
+	@Column(name = "cont_object_id")
+	private Long contObjectId;
+
+	@Column(name = "fias_uuid")
+	@org.hibernate.annotations.Type(type = "org.hibernate.type.PostgresUUIDType")
+	private UUID fiasUUID;
+
+	@Column(name = "fias_full_address")
+	private String fiasFullAddress;
+
+	@Column(name = "geo_json")
+	@Type(type = "StringJsonObject")
+	private String geoJson;
+
+	@Version
+	private int version;
+
+	public Long getContObjectId() {
+		return contObjectId;
+	}
+
+	public void setContObjectId(Long contObjectId) {
+		this.contObjectId = contObjectId;
+	}
+
+	public UUID getFiasUUID() {
+		return fiasUUID;
+	}
+
+	public void setFiasUUID(UUID fiasUUID) {
+		this.fiasUUID = fiasUUID;
+	}
+
+	public String getFiasFullAddress() {
+		return fiasFullAddress;
+	}
+
+	public void setFiasFullAddress(String fiasFullAddress) {
+		this.fiasFullAddress = fiasFullAddress;
+	}
+
+	public String getGeoJson() {
+		return geoJson;
+	}
+
+	public void setGeoJson(String geoJson) {
+		this.geoJson = geoJson;
+	}
+
+	public ContObject getContObject() {
+		return contObject;
+	}
+
+}
---
+++ src/main/java/ru/excbt/datafuse/nmk/data/repository/ContObjectFiasRepository.java
@@ -0,0 +1,9 @@
+package ru.excbt.datafuse.nmk.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ru.excbt.datafuse.nmk.data.model.ContObjectFias;
+
+public interface ContObjectFiasRepository extends CrudRepository<ContObjectFias, Long> {
+
+}
