2aa09a9fe05ced2823dc13e0ba7889336385e3dd
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-07-22 14:35:07 +0300

    ReportTypeKey findByUrlName upgrade
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/report/ReportTypeKey.java
+++ src/main/java/ru/excbt/datafuse/nmk/report/ReportTypeKey.java
@@ -1,8 +1,38 @@
 package ru.excbt.datafuse.nmk.report;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public enum ReportTypeKey {
-	COMMERCE_REPORT, // CONS_REPORT,
-	EVENT_REPORT, CONS_T1_REPORT, CONS_T2_REPORT, METROLOGICAL_REPORT, //
-	CONSUMPTION_REPORT, CONSUMPTION_HISTORY_REPORT, CONSUMPTION_ETALON_REPORT, //
-	LOG_JOURNAL_REPORT
+	COMMERCE_REPORT("commerce"), // CONS_REPORT,
+	EVENT_REPORT("event"), //
+	CONS_T1_REPORT("cons_t1"), //
+	CONS_T2_REPORT("cons_t2"), //
+	METROLOGICAL_REPORT("metrological"), //
+	CONSUMPTION_REPORT("consumption"), //
+	CONSUMPTION_HISTORY_REPORT("consumption_history"), //
+	CONSUMPTION_ETALON_REPORT("consumption_etalon"), //
+	LOG_JOURNAL_REPORT("log_journal");
+
+	private final String urlName;
+
+	private ReportTypeKey(String urlName) {
+		this.urlName = urlName;
+	}
+
+	public String getUrlName() {
+		return urlName;
+	}
+
+	/**
+	 * 
+	 * @param urlName
+	 * @return
+	 */
+	public static ReportTypeKey findByUrlName(String urlName) {
+		Optional<ReportTypeKey> opt = Stream.of(ReportTypeKey.values())
+				.filter((i) -> i.urlName.equals(urlName)).findFirst();
+
+		return opt.isPresent() ? opt.get() : null;
+	}
 }
