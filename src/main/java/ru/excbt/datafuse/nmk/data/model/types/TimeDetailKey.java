f793db34865bb99f9625c3b2b13e27141637f4f4
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-07-23 13:39:03 +0300

    1. SubscrContServiceDataController TimeDetailKey fix 2. TimeDetailKey code refactoring
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/data/model/types/TimeDetailKey.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/types/TimeDetailKey.java
@@ -1,11 +1,13 @@
 package ru.excbt.datafuse.nmk.data.model.types;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import ru.excbt.datafuse.nmk.data.domain.KeynameObject;
 
 public enum TimeDetailKey implements KeynameObject {
 
 	TYPE_24H("24h"), TYPE_1H("1h");
-	//, TYPE_ABS("abs");
 
 	private final String keyname;
 	private final static String ABS_SUFFIX = "_abs";
@@ -14,34 +16,29 @@ public enum TimeDetailKey implements KeynameObject {
 		this.keyname = keyname;
 	}
 
+	@Override
 	public String getKeyname() {
 		return this.keyname;
 	}
 
 	/**
 	 * 
+	 * @return
+	 */
+	public String getAbsPair() {
+		return this.keyname + ABS_SUFFIX;
+	}
+	
+	/**
+	 * 
 	 * @param keyname
 	 * @return
 	 */
 	public static TimeDetailKey searchKeyname(String keyname) {
-		TimeDetailKey result = null;
-		for (TimeDetailKey d : TimeDetailKey.values()) {
-			if (d.keyname.equals(keyname)) {
-				result = d;
-				break;
-			}
-		}
-		return result;
-	}
 
-	public String getAbsPair() {
-		return this.keyname + ABS_SUFFIX;
-		
-//		if (this == TYPE_ABS) {
-//			return "";
-//		} else {
-//
-//			return this.keyname + ABS_SUFFIX;
-//		}
-	}
+		Optional<TimeDetailKey> opt = Stream.of(TimeDetailKey.values())
+				.filter((i) -> i.keyname.equals(keyname)).findFirst();
+
+		return opt.isPresent() ? opt.get() : null;
+	}	
 }
