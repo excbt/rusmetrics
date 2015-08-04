3f68c353bcaff977c8036d09dad93f5e68a7cfc5
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-08-04 17:42:18 +0300

    SystemInfoController readOnlyMode get upgrade
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/web/api/SystemInfoController.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/api/SystemInfoController.java
@@ -1,17 +1,33 @@
 package ru.excbt.datafuse.nmk.web.api;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import ru.excbt.datafuse.nmk.data.model.FullUserInfo;
 import ru.excbt.datafuse.nmk.data.service.support.CurrentUserService;
+import ru.excbt.datafuse.nmk.security.SecuredRoles;
 
 @Controller
 @RequestMapping(value = "/api/systemInfo")
 public class SystemInfoController extends WebApiController {
+
+	private class ReadOnlyMode {
+		final boolean readOnlyMode;
+
+		private ReadOnlyMode(boolean readOnlyMode) {
+			this.readOnlyMode = readOnlyMode;
+		}
+
+		public boolean isReadOnlyMode() {
+			return readOnlyMode;
+		}
+	}
 
 	@Autowired
 	private CurrentUserService currentUserService;
@@ -28,6 +44,29 @@ public class SystemInfoController extends WebApiController {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(result);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/readOnlyMode", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8)
+	public ResponseEntity<?> getUserRights() {
+		UserDetails userDetails = currentUserService.getCurrentUserDetails();
+
+		if (userDetails == null) {
+			return responseOK(new ReadOnlyMode(true));
+		}
+
+		Optional<String> adminRole = userDetails
+				.getAuthorities()
+				.stream()
+				.filter((i) -> SecuredRoles.ROLE_ADMIN.equals(i.getAuthority())
+						|| SecuredRoles.ROLE_SUBSCR_ADMIN.equals(i
+								.getAuthority())).map((i) -> i.getAuthority())
+				.findFirst();
+
+		return responseOK(new ReadOnlyMode(!adminRole.isPresent()));
 	}
 
 }
