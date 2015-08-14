5306b717f8d6249493693366f605cc54f231477a
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-08-14 15:49:25 +0300

    SamlSecurityConfig forceLocalLogin property added
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/web/controller/LoginController.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/controller/LoginController.java
@@ -1,22 +1,42 @@
 package ru.excbt.datafuse.nmk.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ru.excbt.datafuse.nmk.config.security.SamlSecurityConfig;
+
 @Controller
 public class LoginController {
 
+	@Autowired(required = false)
+	private SamlSecurityConfig samlSecurityConfig;
+
+	/**
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/localLogin", method = { RequestMethod.GET,
 			RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT })
 	public ModelAndView doLocalLogin() {
 		return new ModelAndView("login.jsp");
 	}
 
-	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST,
-			RequestMethod.DELETE, RequestMethod.PUT })
-	public String doSamlLogin() {
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = { RequestMethod.GET,
+			RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT })
+	public String doLogin() {
+
+		if (samlSecurityConfig == null
+				|| samlSecurityConfig.isForceLocalLogin()) {
+			return "redirect:/localLogin";
+		}
+
 		return "redirect:/saml/login";
 	}
 }
