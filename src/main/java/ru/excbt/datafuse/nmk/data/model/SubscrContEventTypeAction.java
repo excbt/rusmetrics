bc59c2136c16a51e6b8298d414f1162ea1fd31fc
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2015-12-23 11:51:25 +0300

    SubscrContEventTypeAction upgrade

---
+++ src/main/java/ru/excbt/datafuse/nmk/data/model/SubscrContEventTypeAction.java
@@ -0,0 +1,135 @@
+package ru.excbt.datafuse.nmk.data.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import ru.excbt.datafuse.nmk.data.domain.AbstractAuditableModel;
+import ru.excbt.datafuse.nmk.data.model.markers.DeletableObjectId;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+@Entity
+@Table(name = "subscr_cont_event_type_action")
+public class SubscrContEventTypeAction extends AbstractAuditableModel implements DeletableObjectId {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2322682172623184659L;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "subscriber_id")
+	@JsonIgnore
+	private Subscriber subscriber;
+
+	@Column(name = "subscriber_id", updatable = false, insertable = false)
+	private Long subscriberId;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "cont_event_type_id")
+	@JsonIgnore
+	private ContEventType contEventType;
+
+	@Column(name = "cont_event_type_id", updatable = false, insertable = false)
+	private Long contEventTypeId;
+
+	@Column(name = "subscr_action_user_id")
+	private Long subscrActionUserId;
+
+	@Column(name = "is_email")
+	private Boolean isEmail;
+
+	@Column(name = "is_sms")
+	private Boolean isSms;
+
+	@Version
+	private int version;
+
+	@Column(name = "deleted")
+	private int deleted;
+
+	public Subscriber getSubscriber() {
+		return subscriber;
+	}
+
+	public void setSubscriber(Subscriber subscriber) {
+		this.subscriber = subscriber;
+	}
+
+	public ContEventType getContEventType() {
+		return contEventType;
+	}
+
+	public void setContEventType(ContEventType contEventType) {
+		this.contEventType = contEventType;
+	}
+
+	public Long getSubscrActionUserId() {
+		return subscrActionUserId;
+	}
+
+	public void setSubscrActionUserId(Long subscrActionUserId) {
+		this.subscrActionUserId = subscrActionUserId;
+	}
+
+	public Boolean getIsEmail() {
+		return isEmail;
+	}
+
+	public void setIsEmail(Boolean isEmail) {
+		this.isEmail = isEmail;
+	}
+
+	public Boolean getIsSms() {
+		return isSms;
+	}
+
+	public void setIsSms(Boolean isSms) {
+		this.isSms = isSms;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	@Override
+	public int getDeleted() {
+		return deleted;
+	}
+
+	@Override
+	public void setDeleted(int deleted) {
+		this.deleted = deleted;
+	}
+
+	public Long getSubscriberId() {
+		return subscriberId;
+	}
+
+	public void setSubscriberId(Long subscriberId) {
+		this.subscriberId = subscriberId;
+	}
+
+	public Long getContEventTypeId() {
+		return contEventTypeId;
+	}
+
+	public void setContEventTypeId(Long contEventTypeId) {
+		this.contEventTypeId = contEventTypeId;
+	}
+
+}
--- src/main/java/ru/excbt/datafuse/nmk/data/repository/ContEventTypeRepository.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/repository/ContEventTypeRepository.java
@@ -8,16 +8,15 @@ import org.springframework.data.repository.query.Param;
 
 import ru.excbt.datafuse.nmk.data.model.ContEventType;
 
-public interface ContEventTypeRepository extends
-		CrudRepository<ContEventType, Long> {
+public interface ContEventTypeRepository extends CrudRepository<ContEventType, Long> {
 
-	@Query("SELECT t FROM ContEventType t "
-			+ " WHERE t.isBaseEvent = :isBaseEvent ORDER BY t.name")
-	public List<ContEventType> selectBaseEventTypes(
-			@Param("isBaseEvent") Boolean isBaseEvent);
+	@Query("SELECT t FROM ContEventType t " + " WHERE t.isBaseEvent = :isBaseEvent ORDER BY t.name")
+	public List<ContEventType> selectBaseEventTypes(@Param("isBaseEvent") Boolean isBaseEvent);
 
-	@Query("SELECT t FROM ContEventType t "
-			+ " WHERE t.id IN (:contEventTypes) ")
-	public List<ContEventType> selectContEventTypes(
-			@Param("contEventTypes") List<Long> contEventTypes);
+	@Query("SELECT t FROM ContEventType t " + " WHERE t.id IN (:contEventTypes) ")
+	public List<ContEventType> selectContEventTypes(@Param("contEventTypes") List<Long> contEventTypes);
+
+	@Query("SELECT s FROM ContEventType s WHERE s.isSmsNotification = true")
+	public List<ContEventType> selectBySmsNotification();
+
 }
