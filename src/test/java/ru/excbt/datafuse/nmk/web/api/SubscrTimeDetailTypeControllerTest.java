42fca61ac7294e5bda10ef98fe42f6d42400093a
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-01-28 12:07:15 +0300

    TimeDetailType Service + Controller upgrade

---
+++ src/test/java/ru/excbt/datafuse/nmk/web/api/SubscrTimeDetailTypeControllerTest.java
@@ -0,0 +1,17 @@
+package ru.excbt.datafuse.nmk.web.api;
+
+import org.junit.Test;
+
+import ru.excbt.datafuse.nmk.web.AnyControllerTest;
+
+public class SubscrTimeDetailTypeControllerTest extends AnyControllerTest {
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testTimeDetailType1h24h() throws Exception {
+		_testJsonGet(apiSubscrUrl("/timeDetailType/1h24h"));
+	}
+}
