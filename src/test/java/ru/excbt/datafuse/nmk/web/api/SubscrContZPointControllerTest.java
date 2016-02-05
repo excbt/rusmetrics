517d768c2c86947c678f79eb36c081cae6b67801
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-02-05 13:21:41 +0300

    ContZPointService fix get ContZPointEx for el service

--- src/test/java/ru/excbt/datafuse/nmk/web/api/SubscrContZPointControllerTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/web/api/SubscrContZPointControllerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -30,6 +31,14 @@ public class SubscrContZPointControllerTest extends AnyControllerSubscriberTest 
 		// assertNotNull(result.get(0).getLastDataDate());
 
 		String url = String.format("/api/subscr/contObjects/%d/contZPointsEx", co.getId());
+		_testJsonGet(url);
+
+	}
+
+	@Ignore
+	@Test
+	public void testGetElConsZPointEx() throws Exception {
+		String url = "/api/subscr/contObjects/77921790/contZPointsEx";
 		_testJsonGet(url);
 
 	}
