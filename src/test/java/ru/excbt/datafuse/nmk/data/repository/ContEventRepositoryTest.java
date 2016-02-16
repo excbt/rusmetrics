c43f495d0647f328454ccc4fa15ff705788002e1
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-02-16 14:14:18 +0300

    contEventRepositoryTest some test ignore

--- src/test/java/ru/excbt/datafuse/nmk/data/repository/ContEventRepositoryTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/data/repository/ContEventRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,16 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import com.google.common.collect.Lists;
+
 import ru.excbt.datafuse.nmk.config.jpa.JpaSupportTest;
 import ru.excbt.datafuse.nmk.data.model.ContEvent;
 import ru.excbt.datafuse.nmk.data.service.support.CurrentSubscriberService;
 
-import com.google.common.collect.Lists;
-
 public class ContEventRepositoryTest extends JpaSupportTest {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(ContEventRepositoryTest.class);
+	private static final Logger logger = LoggerFactory.getLogger(ContEventRepositoryTest.class);
 
 	@Autowired
 	private ContEventRepository contEventRepository;
@@ -30,31 +30,33 @@ public class ContEventRepositoryTest extends JpaSupportTest {
 	@Autowired
 	private CurrentSubscriberService currentSubscriberService;
 
+	/**
+	 * 
+	 */
 	@Test
+	@Ignore
 	public void testBySubscriberIdLimit() {
-		
-		long subscriberId = currentSubscriberService
-				.getSubscriberId();
-		
-		Page<ContEvent> events = contEventRepository
-				.selectBySubscriber(subscriberId, new PageRequest(0, 1000));
-		
-		List<ContEvent> list = Lists.newArrayList(events.iterator());
-		
-		assertTrue(list.size() > 0);
-		logger.info("Get {} events by subscriberId:{}", list.size(),
-				subscriberId);
 
-		logger.info("Total elements {} by subscriberId:{}", events.getTotalElements(),
-				subscriberId);
+		long subscriberId = currentSubscriberService.getSubscriberId();
+
+		Page<ContEvent> events = contEventRepository.selectBySubscriber(subscriberId, new PageRequest(0, 1000));
+
+		List<ContEvent> list = Lists.newArrayList(events.iterator());
+
+		assertTrue(list.size() > 0);
+		logger.info("Get {} events by subscriberId:{}", list.size(), subscriberId);
+
+		logger.info("Total elements {} by subscriberId:{}", events.getTotalElements(), subscriberId);
 	}
 
+	/**
+	 * 
+	 */
 	@Test
+	@Ignore
 	public void testBySubscriberWithIds() {
-		Page<ContEvent> events = contEventRepository
-				.selectBySubscriberAndContObjects(
-						currentSubscriberService.getSubscriberId(),
-						Arrays.asList(1L, 2L), new PageRequest(0, 1000));
+		Page<ContEvent> events = contEventRepository.selectBySubscriberAndContObjects(
+				currentSubscriberService.getSubscriberId(), Arrays.asList(1L, 2L), new PageRequest(0, 1000));
 		assertNotNull(events);
 	}
 
