357a9975a8ea7d0a45990ed260660d451e96c2fd
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-04-06 19:34:38 +0300

    Create RMA Subscriber by test

--- src/test/java/ru/excbt/datafuse/nmk/data/service/SubscriberServiceTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/data/service/SubscriberServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,16 +15,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.excbt.datafuse.nmk.config.jpa.JpaSupportTest;
 import ru.excbt.datafuse.nmk.data.model.SubscrRole;
 import ru.excbt.datafuse.nmk.data.model.SubscrUser;
+import ru.excbt.datafuse.nmk.data.model.Subscriber;
 import ru.excbt.datafuse.nmk.data.service.support.CurrentSubscriberService;
 import ru.excbt.datafuse.nmk.data.service.support.PasswordService;
 import ru.excbt.datafuse.nmk.security.SecuredRoles;
 
 public class SubscriberServiceTest extends JpaSupportTest implements SecuredRoles {
 
+	private static final Logger logger = LoggerFactory.getLogger(SubscriberServiceTest.class);
+
 	private final static String ARGON_19 = "test-argon19";
 	private final static String SIMPLE_PASSWORD = "12345";
 
-	private static final Logger logger = LoggerFactory.getLogger(SubscriberServiceTest.class);
+	private final static long RMA_SUBSCRIBER = 103926764L;
 
 	@Autowired
 	private SubscriberService subscriberService;
@@ -37,6 +41,13 @@ public class SubscriberServiceTest extends JpaSupportTest implements SecuredRole
 	@Autowired
 	private CurrentSubscriberService currentSubscriberService;
 
+	@Autowired
+	protected SubscrRoleService subscrRoleService;
+
+	@Autowired
+	private ReportParamsetService reportParamsetService;
+
+	@Ignore
 	@Test
 	public void tesArgon19() {
 		List<SubscrUser> subscrUsers = subscriberService.findUserByUsername(ARGON_19);
@@ -60,6 +71,53 @@ public class SubscriberServiceTest extends JpaSupportTest implements SecuredRole
 		String ldapOu = subscriberService.getRmaLdapOu(id);
 		assertNotNull(ldapOu);
 		logger.info("Rma LDAP ou={}", ldapOu);
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	@Ignore
+	@Test
+	public void testCreateRmaUser() throws Exception {
+
+		SubscrUser subscrUser = new SubscrUser();
+
+		boolean isAdmin = true;
+		boolean isRma = true;
+		boolean isReadonly = false;
+
+		subscrUser.setUserName("rma-77-admin");
+		subscrUser.setFirstName("rma-77-admin");
+		subscrUser.setLastName("rma-77-admin");
+		subscrUser.setSubscriberId(RMA_SUBSCRIBER);
+		subscrUser.getSubscrRoles().clear();
+		subscrUser.setIsAdmin(isAdmin);
+		subscrUser.setIsReadonly(false);
+
+		if (Boolean.TRUE.equals(isReadonly)) {
+			subscrUser.getSubscrRoles().addAll(subscrRoleService.subscrReadonlyRoles());
+			subscrUser.setIsAdmin(false);
+		} else {
+			if (Boolean.TRUE.equals(isAdmin)) {
+				subscrUser.getSubscrRoles().addAll(subscrRoleService.subscrAdminRoles());
+				if (Boolean.TRUE.equals(isRma)) {
+					subscrUser.getSubscrRoles().addAll(subscrRoleService.subscrRmaAdminRoles());
+				}
+			} else {
+				subscrUser.getSubscrRoles().addAll(subscrRoleService.subscrUserRoles());
+			}
+		}
+
+		subscrUserService.createOne(subscrUser, "excbt_12345");
+
+	}
+
+	@Ignore
+	@Test
+	public void testInitDefaultReportParamsets() throws Exception {
+		Subscriber subscriber = subscriberService.findOne(RMA_SUBSCRIBER);
+		reportParamsetService.createDefaultReportParamsets(subscriber);
 	}
 
 }
