9ebf4c8f7d591c5cef414bcb2aa5a82af762de84
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-04-20 14:24:26 +0300

    Revert "Config for Portal-2"
    This reverts commit bb3caf5e6afd527ec72e0effa3bb1e3e56233775.

--- src/main/java/ru/excbt/datafuse/nmk/config/security/WebSecurityConfig.java
+++ src/main/java/ru/excbt/datafuse/nmk/config/security/WebSecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(value = { LocalSecurityConfig.class })
+@Import(value = { SamlSecurityConfig.class })
 public class WebSecurityConfig {
 
 }
