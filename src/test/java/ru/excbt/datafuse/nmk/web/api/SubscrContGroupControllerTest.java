d6b8ec4b683c2a0c63c91fb685476198480aa525
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-05-11 14:10:03 +0300

    SubscrContGroupControllerTest refactoring

--- src/test/java/ru/excbt/datafuse/nmk/web/api/SubscrContGroupControllerTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/web/api/SubscrContGroupControllerTest.java
@@ -1,10 +1,6 @@
 package ru.excbt.datafuse.nmk.web.api;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.testSecurityContext;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
@@ -12,17 +8,12 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.jayway.jsonpath.JsonPath;
 
 import ru.excbt.datafuse.nmk.data.model.ContGroup;
 import ru.excbt.datafuse.nmk.data.service.ContGroupService;
 import ru.excbt.datafuse.nmk.data.service.support.CurrentSubscriberService;
 import ru.excbt.datafuse.nmk.web.AnyControllerTest;
+import ru.excbt.datafuse.nmk.web.RequestExtraInitializer;
 
 public class SubscrContGroupControllerTest extends AnyControllerTest {
 
@@ -34,50 +25,50 @@ public class SubscrContGroupControllerTest extends AnyControllerTest {
 	@Autowired
 	private ContGroupService contGroupService;
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testContGroupObjects() throws Exception {
 		_testGetJson("/api/subscr/contGroup/0/contObject/available");
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testContGroup() throws Exception {
 		_testGetJson("/api/subscr/contGroup");
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testCreateContGroup() throws Exception {
 
 		ContGroup group = new ContGroup();
 		group.setContGroupName("Новая группа");
-		String jsonBody = null;
-		try {
-			jsonBody = OBJECT_MAPPER.writeValueAsString(group);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-			fail();
-		}
 
 		long[] objectIds = { 18811504L, 18811505L };
 
-		logger.info("Array of {}", arrayToString(objectIds));
-		logger.info("jsonBody: {}", jsonBody);
+		RequestExtraInitializer params = (builder) -> {
+			builder.param("contObjectIds", arrayToString(objectIds));
+		};
 
-		ResultActions resultAction = mockMvc.perform(
-				org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post("/api/subscr/contGroup")
-						.contentType(MediaType.APPLICATION_JSON).param("contObjectIds", arrayToString(objectIds))
-						.content(jsonBody).with(testSecurityContext()).accept(MediaType.APPLICATION_JSON));
+		Long id = _testCreateJson("/api/subscr/contGroup", group, params);
 
-		resultAction.andDo(MockMvcResultHandlers.print());
+		testDeleteContGroup(id);
 
-		resultAction.andExpect(status().isCreated());
-
-		String jsonContent = resultAction.andReturn().getResponse().getContentAsString();
-		Integer createdId = JsonPath.read(jsonContent, "$.id");
-		logger.info("createdId: {}", createdId);
-
-		testDeleteContGroup(createdId.longValue());
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testUpdateContGroup() throws Exception {
 		List<ContGroup> contGroups = contGroupService
@@ -93,38 +84,20 @@ public class SubscrContGroupControllerTest extends AnyControllerTest {
 
 		long[] objectIds = { 18811522L, 18811533L };
 
-		logger.info("Array of {}", arrayToString(objectIds));
-
 		cg.setContGroupComment("TEST AutoUpdate " + System.currentTimeMillis());
-		String jsonBody = OBJECT_MAPPER.writeValueAsString(cg);
 		String urlStr = "/api/subscr/contGroup/" + cg.getId();
 
-		ResultActions resultActionsAll;
-		try {
-			resultActionsAll = mockMvc.perform(put(urlStr).contentType(MediaType.APPLICATION_JSON).content(jsonBody)
-					.param("contObjectIds", arrayToString(objectIds)).with(testSecurityContext())
-					.accept(MediaType.APPLICATION_JSON));
+		RequestExtraInitializer params = (builder) -> {
+			builder.param("contObjectIds", arrayToString(objectIds));
+		};
 
-			resultActionsAll.andDo(MockMvcResultHandlers.print());
-
-			resultActionsAll.andExpect(status().isOk());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.toString());
-		}
+		_testUpdateJson(urlStr, cg, params);
 	}
 
-	/*
-	 * @Test
+	/**
 	 * 
-	 * @Ignore public void testDeleteContGroup() throws Exception {
-	 * List<ContGroup> contGroups = contGroupService
-	 * .selectSubscriberGroups(currentSubscriberService .getSubscriberId());
-	 * 
-	 * assertTrue(contGroups.size() > 0); ContGroup cg; if (contGroups.size() >
-	 * 2) { cg = contGroups.get(1); } else { cg = contGroups.get(0); } String
-	 * urlStr = "/api/contGroup/" + cg.getId(); testJsonDelete(urlStr); }
+	 * @param contGroupId
+	 * @throws Exception
 	 */
 
 	public void testDeleteContGroup(Long contGroupId) throws Exception {
