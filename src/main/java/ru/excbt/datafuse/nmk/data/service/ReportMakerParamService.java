758943c3bc0b86e210cfed4066f254b6d1a03c34
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-05-17 11:51:38 +0300

    ReportMakerParamService reportMakerParamFactory update & code cleanup

--- src/main/java/ru/excbt/datafuse/nmk/data/service/ReportMakerParamService.java
+++ src/main/java/ru/excbt/datafuse/nmk/data/service/ReportMakerParamService.java
@@ -3,9 +3,7 @@ package ru.excbt.datafuse.nmk.data.service;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -18,15 +16,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.excbt.datafuse.nmk.config.jpa.TxConst;
-import ru.excbt.datafuse.nmk.data.model.ReportMetaParamCommon;
 import ru.excbt.datafuse.nmk.data.model.ReportMetaParamSpecial;
 import ru.excbt.datafuse.nmk.data.model.ReportParamset;
 import ru.excbt.datafuse.nmk.data.model.ReportParamsetParamSpecial;
 import ru.excbt.datafuse.nmk.data.model.keyname.ReportType;
 import ru.excbt.datafuse.nmk.data.model.support.ReportMakerParam;
-import ru.excbt.datafuse.nmk.data.model.types.ReportMetaParamSpecialTypeName;
 import ru.excbt.datafuse.nmk.data.service.support.SubscriberParam;
-import ru.excbt.datafuse.nmk.report.ReportPeriodKey;
 import ru.excbt.datafuse.nmk.report.ReportTypeKey;
 
 /**
@@ -41,6 +36,9 @@ import ru.excbt.datafuse.nmk.report.ReportTypeKey;
 public class ReportMakerParamService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ReportMakerParamService.class);
+
+	private static final boolean PREVIEW_ON = true;
+	private static final boolean PREVIEW_OFF = false;
 
 	@Autowired
 	private ReportParamsetService reportParamsetService;
@@ -63,7 +61,7 @@ public class ReportMakerParamService {
 	public ReportMakerParam newReportMakerParam(long reportParamsetId) {
 		ReportParamset reportParamset = reportParamsetService.findReportParamset(reportParamsetId);
 
-		return newReportMakerParam(reportParamset, null, false);
+		return reportMakerParamFactory(reportParamset, null, PREVIEW_OFF);
 
 	}
 
@@ -76,7 +74,7 @@ public class ReportMakerParamService {
 	public ReportMakerParam newReportMakerParam(long reportParamsetId, boolean previewMode) {
 		ReportParamset reportParamset = reportParamsetService.findReportParamset(reportParamsetId);
 
-		return newReportMakerParam(reportParamset, null, previewMode);
+		return reportMakerParamFactory(reportParamset, null, previewMode);
 
 	}
 
@@ -88,7 +86,7 @@ public class ReportMakerParamService {
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newReportMakerParam(long reportParamsetId, Long[] contObjectIds) {
 		ReportParamset reportParamset = reportParamsetService.findReportParamset(reportParamsetId);
-		return newReportMakerParam(reportParamset, contObjectIds, false);
+		return reportMakerParamFactory(reportParamset, contObjectIds, PREVIEW_OFF);
 	}
 
 	/**
@@ -99,7 +97,7 @@ public class ReportMakerParamService {
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newReportMakerParam(long reportParamsetId, Long[] contObjectIds, boolean previewMode) {
 		ReportParamset reportParamset = reportParamsetService.findReportParamset(reportParamsetId);
-		return newReportMakerParam(reportParamset, contObjectIds, previewMode);
+		return reportMakerParamFactory(reportParamset, contObjectIds, previewMode);
 	}
 
 	/**
@@ -110,7 +108,7 @@ public class ReportMakerParamService {
 	 */
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newReportMakerParam(ReportParamset reportParamset, Long[] contObjectIds) {
-		return newReportMakerParam(reportParamset, contObjectIds, false);
+		return reportMakerParamFactory(reportParamset, contObjectIds, PREVIEW_OFF);
 	}
 
 	/**
@@ -122,7 +120,7 @@ public class ReportMakerParamService {
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newSubscriberReportMakerParam(SubscriberParam subscriberParam,
 			ReportParamset reportParamset, Long[] contObjectIds) {
-		return newReportMakerParam(reportParamset, contObjectIds, false);
+		return reportMakerParamFactory(reportParamset, contObjectIds, PREVIEW_OFF);
 	}
 
 	/**
@@ -133,7 +131,7 @@ public class ReportMakerParamService {
 	 */
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newReportMakerParam(ReportParamset reportParamset) {
-		return newReportMakerParam(reportParamset, null, false);
+		return reportMakerParamFactory(reportParamset, null, PREVIEW_OFF);
 	}
 
 	/**
@@ -144,6 +142,20 @@ public class ReportMakerParamService {
 	 */
 	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
 	public ReportMakerParam newReportMakerParam(ReportParamset reportParamset, Long[] contObjectIds,
+			boolean previewMode) {
+
+		return reportMakerParamFactory(reportParamset, contObjectIds, previewMode);
+
+	}
+
+	/**
+	 * 
+	 * @param reportParamset
+	 * @param contObjectIds
+	 * @param previewMode
+	 * @return
+	 */
+	private ReportMakerParam reportMakerParamFactory(ReportParamset reportParamset, Long[] contObjectIds,
 			boolean previewMode) {
 
 		checkNotNull(reportParamset);
@@ -173,189 +185,6 @@ public class ReportMakerParamService {
 
 		return new ReportMakerParam(reportParamset, resultContObjectIdList, subscrContObjectIds, previewMode);
 
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
-	public boolean isAllCommonRequiredParamsExists2(ReportMakerParam reportMakerParam) {
-
-		checkNotNull(reportMakerParam);
-		// checkState(reportMakerParam.isParamsetValid());
-
-		boolean result = true;
-
-		ReportParamset reportParamset = reportMakerParam.getReportParamset();
-
-		ReportType reportType = reportParamset.getReportTemplate().getReportType();
-
-		ReportMetaParamCommon paramCommon = reportType.getReportMetaParamCommon();
-
-		if (reportParamset.getReportPeriodKey() == ReportPeriodKey.INTERVAL) {
-
-			result = result && checkRequiredParamNotNull(paramCommon.getStartDateRequired(),
-					reportParamset.getParamsetStartDate());
-
-			result = result
-					&& checkRequiredParamNotNull(paramCommon.getEndDateRequired(), reportParamset.getParamsetEndDate());
-		}
-
-		if (reportParamset.getReportPeriodKey() == ReportPeriodKey.DAY) {
-			result = result
-					&& checkRequiredParamNotNull(paramCommon.getOneDateRequired(), reportParamset.getParamsetOneDate());
-		}
-
-		// Only one object required
-		if (Boolean.TRUE.equals(paramCommon.getOneContObjectRequired())
-				&& Boolean.FALSE.equals(paramCommon.getManyContObjectsRequired())) {
-			result = result && reportMakerParam.getReportContObjectIds().size() == 1;
-		}
-
-		// More than 0 Objects required
-		if (Boolean.TRUE.equals(paramCommon.getOneContObjectRequired())
-				&& Boolean.TRUE.equals(paramCommon.getManyContObjectsRequired())) {
-			result = result && reportMakerParam.getReportContObjectIds().size() > 0;
-		}
-
-		// More than 1 object required
-		if (Boolean.FALSE.equals(paramCommon.getOneContObjectRequired())
-				&& Boolean.TRUE.equals(paramCommon.getManyContObjectsRequired())) {
-			result = result && reportMakerParam.getReportContObjectIds().size() > 1;
-		}
-
-		return result;
-	}
-
-	/**
-	 * 
-	 * @param reportMakerParam
-	 * @return
-	 */
-	@Transactional(value = TxConst.TX_DEFAULT, readOnly = true)
-	public boolean isAllSpecialRequiredParamsExists2(ReportMakerParam reportMakerParam) {
-
-		checkNotNull(reportMakerParam);
-		// checkState(reportMakerParam.isParamsetValid());
-
-		boolean result = true;
-
-		ReportParamset reportParamset = reportMakerParam.getReportParamset();
-
-		ReportType reportType = reportParamset.getReportTemplate().getReportType();
-
-		checkNotNull(reportType.getReportMetaParamSpecialList());
-
-		Collection<ReportMetaParamSpecial> specialParamDefs = reportType.getReportMetaParamSpecialList();
-
-		logger.debug("specialParamDefs. size: {}", specialParamDefs.size());
-		logger.debug("paramSpecialList.size: {}", reportParamset.getParamSpecialList().size());
-
-		List<ReportParamsetParamSpecial> paramValues = new ArrayList<>();
-		paramValues.addAll(reportParamset.getParamSpecialList());
-
-		for (ReportMetaParamSpecial paramDef : specialParamDefs) {
-			logger.debug("Checking paramDef: {}", paramDef.getParamSpecialKeyname());
-			if (result && Boolean.TRUE.equals(paramDef.getParamSpecialRequired())) {
-				logger.debug("paramDef. id:{} keyname:{} ({}) .... required: {}", paramDef.getId(),
-						paramDef.getParamSpecialKeyname(), paramDef.getParamSpecialCaption(),
-						paramDef.getParamSpecialRequired());
-
-				boolean checkRequired = false;
-
-				logger.debug("paramValues.size: {}", paramValues.size());
-
-				List<ReportParamsetParamSpecial> currCheckValues = new ArrayList<>();
-				currCheckValues.addAll(paramValues);
-
-				for (ReportParamsetParamSpecial checkValue : currCheckValues) {
-
-					if (paramDef.getId().equals(checkValue.getReportMetaParamSpecialId())) {
-
-						// String paramTypeKeyname =
-						// paramDef.getParamSpecialType().getKeyname();
-						String paramTypeName = paramDef.getParamSpecialType().getSpecialTypeName();
-
-						logger.debug("Found param value id: {}. paramDef.id: {} paramTypeKeyname: {}",
-								checkValue.getId(), paramDef.getId(), paramDef.getParamSpecialKeyname());
-
-						if (paramTypeName == null) {
-							break;
-						}
-						ReportMetaParamSpecialTypeName typeName = ReportMetaParamSpecialTypeName.valueOf(paramTypeName);
-
-						checkRequired = checkParamSpecialFieldValue(typeName, checkValue);
-
-						// logger.debug("Remove: {}",
-						// paramValues.remove(paramValue));
-						paramValues.remove(checkValue);
-						// break;
-
-					}
-				}
-
-				result = result && checkRequired;
-			}
-		}
-
-		return result;
-	}
-
-	/**
-	 * 
-	 * @param isRequred
-	 * @param obj
-	 * @return
-	 */
-	private boolean checkRequiredParamNotNull(Boolean isRequired, Object obj) {
-		if (Boolean.TRUE.equals(isRequired)) {
-			return (obj != null);
-		}
-		return true;
-	}
-
-	/**
-	 * 
-	 * @param typeKey
-	 * @param param
-	 * @return
-	 */
-	private boolean checkParamSpecialFieldValue(ReportMetaParamSpecialTypeName typeName,
-			ReportParamsetParamSpecial param) {
-
-		boolean result = false;
-
-		switch (typeName) {
-		case STRING:
-			result = param.getTextValue() != null;
-			break;
-		case DATE:
-			result = param.getOneDateValue() != null;
-			break;
-		case DATETIME:
-			result = param.getOneDateValue() != null;
-			break;
-		case BOOL:
-			result = param.getBoolValue() != null;
-			break;
-		case NUMERIC:
-			result = param.getNumericValue() != null;
-			break;
-		case DATE_PERIOD:
-			result = param.getStartDateValue() != null && param.getEndDateValue() != null;
-			break;
-		case DATETIME_PERIOD:
-			result = param.getStartDateValue() != null && param.getEndDateValue() != null;
-			break;
-		case DIRECTORY:
-			result = param.getDirectoryValue() != null;
-			break;
-		default:
-			break;
-		}
-
-		return result;
 	}
 
 	/**
