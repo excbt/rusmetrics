4b47363396b5b3d1a861dcf7dc264c681d8796e2
a.artamonov <a.artamonov@excbt.ru>
2016-07-12 12:16:49 +0300

    Add creation report modal window.
    Signed-off-by: a.artamonov <a.artamonov@excbt.ru>

---
+++ src/main/webapp/app/scripts/directives/nmc-creation-report-modal.js
@@ -0,0 +1,20 @@
+angular.module('portalNMC')
+.directive('nmcCreationReportModal', function(){
+    return {
+        replace: true,
+        templateUrl: "scripts/directives/templates/nmc-creation-report-modal.html",
+        controller: function($scope, mainSvc){
+            
+            //**********************************************************
+            //Creation report window
+            //**********************************************************
+                        
+            $('#creationReportModal').on('hidden.bs.modal', function(){
+            	$scope.createReportWithParamsRequestCancel();
+//                $scope.createReportCancel();
+            	$scope.createReportWithParamsInProgress = false;
+            	$scope.createReportProgress = false;
+            });
+        }
+    }
+});
