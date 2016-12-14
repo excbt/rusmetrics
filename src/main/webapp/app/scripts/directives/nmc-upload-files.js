e0ffcc38c9c6c200be12291f55763af577a52b2b
Alexander Artamonov <a.artamonov@excbt.ru>
2016-12-14 14:44:08 +0300

    upgrade upload files interface

--- src/main/webapp/app/scripts/directives/nmc-upload-files.js
+++ src/main/webapp/app/scripts/directives/nmc-upload-files.js
@@ -6,8 +6,11 @@ angular.module("portalNMC")
         return {
             replace: true,
             restrict: "AEC",
+            scope: true,
             templateUrl: "scripts/directives/templates/nmc-upload-files-template.html",
             controller: ['$scope', 'FileUploader', 'mainSvc', 'notificationFactory', '$timeout', function ($scope, FileUploader, mainSvc, notificationFactory, $timeout) {
+//console.log($scope);                
+                $scope.serverUrl = "../api/subscr/service/datahwater/contObjects/upload";
                 
                 function errorCallback(e) {
                     var errorObj = mainSvc.errorCallbackHandler(e);
@@ -17,9 +20,13 @@ angular.module("portalNMC")
                 //file upload settings
                 $scope.initFileUploader =  function () {
 //console.log("initFileUploader");                    
-                    var url = "";
-                    $scope.uploader = new FileUploader({url: url});
+//                    var url = "../api/subscr/service/datahwater/contObjects/upload";
+                    $scope.uploader = new FileUploader({url: $scope.serverUrl});
+//                    $scope.uploader = new FileUploader();
+//console.log($scope.uploader);
+//console.log(typeof $scope.uploader);                    
                     $scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
+//console.log("onErrorItem");                        
                         console.info('onErrorItem', status, response);
                         errorCallback(response);
                         $scope.successOnUpload = false;
@@ -27,14 +34,21 @@ angular.module("portalNMC")
                         $scope.uploadFlag = false;
                         //notificationFactory.errorInfo(response.resultCode, response.description);
                     };
+                    
+//                    $scope.uploader.onBeforeUploadItem(function (item) {
+//                        item.url = $scope.serverUrl;
+//console.log($scope.serverUrl);
+//                    });
 
                     $scope.uploader.onSuccessItem = function (item, response, status, headers) {
             //console.log(item);
                     };
                     $scope.uploader.onCompleteAll = function () {
+//console.log("onCompleteAll");                        
 //                        $scope.getData(1);
-                        $scope.successOnUpload = true;
-                        $scope.errorOnUpload = false;
+                        $scope.onUploadAll = true;
+//                        $scope.successOnUpload = true;
+//                        $scope.errorOnUpload = false;
             //            $scope.$apply();
                     };
 
@@ -52,14 +66,35 @@ angular.module("portalNMC")
                         $("#upLoadFilesModal").on('hidden.bs.modal', function () {
 //console.log("upLoadFileModal hidden");        
 //                            $scope.uploader = null;
+                            $scope.uploader.clearQueue();
                             $scope.uploadFlag = false;
                             $scope.successOnUpload = false;
                             $scope.errorOnUpload = false;
+                            $scope.onUploadAll = true;
                             $scope.$apply();
                         });
                     }, 10);
                     
                 };
+                
+                //Upload file with the indicator data to the server
+                $scope.uploadFiles = function () {
+//console.log("uploadFiles: " +$scope.serverUrl);
+//                    $scope.uploader.url = $scope.serverUrl;                                        
+                    $scope.uploadFlag = true;
+//console.log($scope.uploader);
+                    $scope.uploader.uploadAll();
+                };
+                
+//                $scope.$watch('serverUrl', function (newval, oldval) {
+//                    console.log("watch ");
+//                    console.log(newval);
+//                    console.log(oldval);
+//                    $scope.uploader.onBeforeUploadItem(function (item) {
+//                        item.url = $scope.serverUrl;
+//console.log($scope.serverUrl);
+//                    });
+//                });
                 
                 function initDirective() {
 //console.log("init directive");                    
