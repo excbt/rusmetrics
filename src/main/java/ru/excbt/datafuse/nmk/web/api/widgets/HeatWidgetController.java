c6573c4ebb20810d3888da1174f670c5318eadc2
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2016-12-29 11:36:03 +0300

    Heat Wizard add month & 24 modes
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

--- src/main/java/ru/excbt/datafuse/nmk/web/api/widgets/HeatWidgetController.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/api/widgets/HeatWidgetController.java
@@ -33,7 +33,7 @@ import ru.excbt.datafuse.nmk.utils.LocalDateUtils;
 @RequestMapping(value = "/api/subscr/widgets/heat/{contZpointId}")
 public class HeatWidgetController extends WidgetController {
 
-	private final static String[] availableModes = { "TODAY", "YESTERDAY", "WEEK" };
+	private final static String[] availableModes = { "TODAY", "YESTERDAY", "WEEK", "MONTH", "24" };
 
 	@Autowired
 	private HeatWidgetService heatWidgetService;
