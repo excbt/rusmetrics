bfbcc44f442c67f07242927b1afd921a950140e3
Alexander Artamonov <a.artamonov@excbt.ru>
2017-01-09 19:08:06 +0300

    Upgrade zpoint heat widgets.

--- src/main/webapp/app/widgets/zpointHeat/zpointHeat.js
+++ src/main/webapp/app/widgets/zpointHeat/zpointHeat.js
@@ -20,7 +20,9 @@ angular.module('zpointHeatWidget', ['angularWidget', 'chart.js', 'ngCookies'])
             monthsShort : "янв._фев._март_апр._май_июнь_июль_авг._сен._окт._ноя._дек.".split("_")
         });
         var DATA_URL = "../api/subscr/widgets/heat",/*//chart/heatTemp";*/
-            ZPOINT_STATUS_TEMPLATE = "widgets/zpointHeat/zpoint-state-";
+            ZPOINT_STATUS_TEMPLATE = "widgets/zpointHeat/zpoint-state-",
+            SERVER_DATE_FORMAT = "DD-MM-YYYY HH:mm",
+            USER_DATE_FORMAT = "DD.MM.YYYY HH:mm";
         
         $scope.widgetOptions = widgetConfig.getOptions();
         //console.log($scope.widgetOptions);
@@ -69,10 +71,95 @@ angular.module('zpointHeatWidget', ['angularWidget', 'chart.js', 'ngCookies'])
         $scope.data.contZpointId = $scope.widgetOptions.contZpointId;
     
         $scope.presentDataFlag = false;
+    
+        var customTooltips = function (tooltip) {
+			// Tooltip Element
+			var tooltipEl = document.getElementById('chartjs-tooltip');
+            
+			if (!tooltipEl) {
+				tooltipEl = document.createElement('div');
+				tooltipEl.id = 'chartjs-tooltip';
+				tooltipEl.innerHTML = "<table></table>";
+				document.body.appendChild(tooltipEl);
+			}
+
+			// Hide if no tooltip
+			if (tooltip.opacity === 0) {
+				tooltipEl.style.opacity = 0;
+				return;
+			}
+
+			// Set caret Position
+			tooltipEl.classList.remove('above', 'below', 'no-transform');
+			if (tooltip.yAlign) {
+				tooltipEl.classList.add(tooltip.yAlign);
+			} else {
+				tooltipEl.classList.add('no-transform');
+			}
+
+			function getBody(bodyItem) {
+				return bodyItem.lines;
+			}
+
+			// Set Text
+			if (tooltip.body) {
+//                console.log(tooltip);
+				var titleLines = tooltip.title || [];
+				var bodyLines = tooltip.body.map(getBody);
+                var index = null;
+                if (tooltip.dataPoints && tooltip.dataPoints.length > 0) {
+                    index = tooltip.dataPoints[0].index;
+                }                
+
+				var innerHtml = '<thead>';
+                if (index !== null && $scope.lineChart.dataTitle.length > 0 && $scope.lineChart.dataTitle[index].dataDateString !== null) {
+                    innerHtml += '<tr><th>' + moment($scope.lineChart.dataTitle[index].dataDateString, SERVER_DATE_FORMAT).format(USER_DATE_FORMAT) + '</th></tr>';
+                } else {
+                    titleLines.forEach(function (title) {
+                        innerHtml += '<tr><th>' + title + '</th></tr>';
+                    });
+                }
+                if (index !== null && $scope.lineChart.dataTitle.length > 0 && $scope.lineChart.dataTitle[index].t_ambiance !== null) {
+                    innerHtml += '<tr><th>' + 't <sub>окр.</sub> = ' + $scope.lineChart.dataTitle[index].t_ambiance + '&deg;C</th></tr>';
+                }
+                
+				innerHtml += '</thead><tbody>';
+
+				bodyLines.forEach(function (body, i) {
+					var colors = tooltip.labelColors[i];
+					var style = 'background:' + colors.backgroundColor;
+					style += '; border-color:' + colors.borderColor;
+					style += '; border-width: 2px';
+					var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+					innerHtml += '<tr><td>' + span + body + '&deg;C</td></tr>';
+				});
+				innerHtml += '</tbody>';
+
+				var tableRoot = tooltipEl.querySelector('table');
+//                console.log(tooltipEl);
+//				console.log(tableRoot);
+				tableRoot.innerHTML = innerHtml;
+			}
+
+			var position = this._chart.canvas.getBoundingClientRect();
+//            console.log(this);
+//			console.log(this._chart);
+
+			// Display, position, and set styles for font
+			tooltipEl.style.opacity = 1;
+			tooltipEl.style.left = position.left + tooltip.caretX + 'px';
+			tooltipEl.style.top = position.top + tooltip.caretY + 'px';
+			tooltipEl.style.fontFamily = tooltip._fontFamily;
+			tooltipEl.style.fontSize = tooltip.fontSize;
+			tooltipEl.style.fontStyle = tooltip._fontStyle;
+			tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+		};
+    
         $scope.lineChart = {};
         $scope.lineChart.labels = [];//["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
         $scope.lineChart.series = ['T норм', 'T факт'];
         $scope.lineChart.data = [[], []];
+        $scope.lineChart.dataTitle = [];
 //        var tmpData = [
 //            [65, 59, 80, 81, 56, 55, 40],
 //            [28, 48, 40, 19, 86, 27, 90]
@@ -86,8 +173,32 @@ angular.module('zpointHeatWidget', ['angularWidget', 'chart.js', 'ngCookies'])
                 display: true
             },
             tooltips: {
-                beforeTitle: function (arr, data) {
-                    return "tooltip";
+                /*enabled: false,
+                mode: 'index',
+                position: 'nearest',
+                custom: customTooltips
+                */
+                callbacks: {
+                    beforeTitle: function (arr, data) {
+//                        console.log(arr);
+//                        console.log(data);
+                        var result = "";
+                        if (angular.isArray(arr) && arr.length > 0 && $scope.lineChart.dataTitle.length > 0 && $scope.lineChart.dataTitle[arr[0].index].dataDateString !== null) {
+                            result = moment($scope.lineChart.dataTitle[arr[0].index].dataDateString, SERVER_DATE_FORMAT).format(USER_DATE_FORMAT);
+                        }
+                        return result;
+                    },
+                    title: function (arr, data) {
+//                        console.log(arr);
+//                        console.log(data);
+                        var result = "";
+                        if (angular.isArray(arr) && arr.length > 0 && $scope.lineChart.dataTitle.length > 0 && $scope.lineChart.dataTitle[arr[0].index].t_ambiance !== null) {
+                            result = "t окр. = " + $scope.lineChart.dataTitle[arr[0].index].t_ambiance;
+                        }
+                        return result;
+//                        return "t окр. = " + arr[2].yLabel + " C";
+                    }
+                    
                 }
             }
         };
@@ -123,10 +234,17 @@ angular.module('zpointHeatWidget', ['angularWidget', 'chart.js', 'ngCookies'])
             $scope.presentDataFlag = true;
             $scope.lineChart.labels = [];
             $scope.lineChart.data = [[], []];
-            rsp.data.forEach(function (elm) {                
-                $scope.lineChart.labels.push(moment(elm.dataDateString, "DD-MM-YYYY HH:mm").format($scope.data.currentMode.dateFormat));
+            $scope.lineChart.dataTitle = [];
+            var dataTitleElem = {};
+            rsp.data.forEach(function (elm) {
+                $scope.lineChart.labels.push(moment(elm.dataDateString, SERVER_DATE_FORMAT).format($scope.data.currentMode.dateFormat));
                 $scope.lineChart.data[0].push(elm.chartT_in);
                 $scope.lineChart.data[1].push(elm.t_in);
+                dataTitleElem = {
+                    dataDateString : elm.dataDateString,
+                    t_ambiance: elm.t_ambiance
+                };
+                $scope.lineChart.dataTitle.push(dataTitleElem);
             });
         }
     
@@ -150,6 +268,11 @@ angular.module('zpointHeatWidget', ['angularWidget', 'chart.js', 'ngCookies'])
             }
             if (angular.isDefined(resp.data.forecastTemp) && resp.data.forecastTemp !== null) {
                 $scope.data.forecastTemp = resp.data.forecastTemp;
+                if ($scope.data.forecastTemp <= 0) {
+                    $scope.data.forecastTempColor = "#337ab7";
+                } else {
+                    $scope.data.forecastTempColor = "#ef473a";
+                }
             } else {
                 console.log("zpointHeatWidget: forecast temperature is empty.");
             }
