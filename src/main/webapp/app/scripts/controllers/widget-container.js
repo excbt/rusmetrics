1f36a0a26bb96bb7c7becb407f869a2a60d28692
Alexander Artamonov <a.artamonov@excbt.ru>
2017-01-17 15:11:37 +0300

    Fix widget view.

---
+++ src/main/webapp/app/scripts/controllers/widget-container.js
@@ -0,0 +1,48 @@
+/*jslint node: true*/
+/*global angular*/
+'use strict';
+angular.module('portalNMC')
+    .controller('widgetContainer', function ($scope) {
+        $scope.isLoading = true;
+
+        $scope.$on('exportPropertiesUpdated', function (event, props) {
+//            console.log('exportPropertiesUpdated');
+//            console.log(event);
+//            console.log(props);
+            if (angular.isDefined(props) && angular.isDefined(props.title)) {
+                $scope.title = props.title;
+            }
+            if (angular.isDefined(props) && angular.isDefined(props.contObjectId) && angular.isDefined(props.contZpointId) && angular.isDefined(props.action) && props.action === "openIndicators") {
+//                console.log('on export getindicators');
+                if (angular.isDefined($scope.getIndicators)) {
+                    $scope.getIndicators(Number(props.contObjectId), Number(props.contZpointId));
+                }
+            }
+            if (angular.isDefined(props) && angular.isDefined(props.contObjectId) && angular.isDefined(props.action) && props.action === "openNotices") {
+                if (angular.isDefined($scope.openNotices)) {
+                    $scope.openNotices(Number(props.contObjectId));
+                }
+            }
+        });
+        $scope.$on('requestToGetIndicators', function (event, props) {
+//            console.log('on requestToGetIndicators');
+            if (angular.isDefined($scope.getIndicators)) {
+                $scope.getIndicators(props.contObjectId, props.contZpointId);
+            }
+        });
+    
+        $scope.$on('widgetLoaded', function () {
+            $scope.isLoading = false;
+            $scope.isError = false;
+        });
+        $scope.$on('widgetError', function () {
+            $scope.isLoading = false;
+            $scope.isError = true;
+        });
+
+        $scope.reload = function () {
+            $scope.isLoading = true;
+            $scope.isError = false;
+            $scope.$broadcast('reloadWidget');
+        };
+    });
