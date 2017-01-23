01170bf231c18c9fcaff1632c1275e597323e056
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-01-23 12:52:15 +0300

    mvn-install-jara added
    & jasperreports-functions-6.3.0.jar added
    
    Signed-off-by: A.Kovtonyuk <a.kovtonyk@excbt.ru>

---
+++ mvn-install-jars.bat
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=./libs/ru/excbt/jasper/fonts_rus/1.0/fonts_rus-1.0.jar -DgroupId=ru.excbt.jasper -DartifactId=fonts_rus -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./libs/ru/excbt/jasper/jasperreports-functions/6.3.0/jasperreports-functions-6.3.0.jar -DgroupId=ru.excbt.jasper -DartifactId=jasperreports-functions -Dversion=6.3.0 -Dpackaging=jar
