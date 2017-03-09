8a763d701f33de4c5edec7581ab8d2da59210514
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-03-09 14:24:43 +0300

    Ldap properties upgrade

--- src/main/java/ru/excbt/datafuse/nmk/config/ldap/LdapConfig.java
+++ src/main/java/ru/excbt/datafuse/nmk/config/ldap/LdapConfig.java
@@ -1,60 +1,52 @@
 package ru.excbt.datafuse.nmk.config.ldap;
 
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import ru.excbt.datafuse.nmk.config.ldap.LdapConfig.LdapProps;
 
 @Configuration
 @ComponentScan(basePackages = { "ru.excbt.datafuse.nmk.ldap" })
-@PropertySource(value = "classpath:META-INF/ldap.properties")
-public class LdapConfig {
+@EnableConfigurationProperties(value = {LdapProps.class})
+    public class LdapConfig {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(LdapConfig.class);
-	
-	@Value("${ldap.url}")
-	private String ldapUrl;
 
-	@Value("${ldap.base}")
-	private String ldapBase;
+    @Data
+    @ConfigurationProperties(prefix = "ldap")
+    public static class LdapProps {
+        private String url;
+        private String base;
+        private String user;
+        private String password;
+    }
 
-	@Value("${ldap.user}")
-	private String ldapUser;
-
-	@Value("${ldap.password}")
-	private String ldapPassword;
-
-	/**
-	 * 
-	 * @return
-	 */
-	public String getLdapBase() {
-		return ldapBase;
-	}
-	
 	@Bean
-    public LdapContextSource contextSource () {
+    public LdapContextSource contextSource (LdapProps ldapProps) {
 		logger.info("Connecting to LDAP");
-		logger.info("LDAP url: {}", ldapUrl);
-		logger.info("LDAP ldapBase: {}", ldapBase);
-		logger.info("LDAP ldapUser: {}", ldapUser);
+		logger.info("LDAP url: {}", ldapProps.url);
+		logger.info("LDAP ldapBase: {}", ldapProps.base);
+		logger.info("LDAP ldapUser: {}", ldapProps.user);
         LdapContextSource contextSource= new LdapContextSource();
-        contextSource.setUrl(ldapUrl);
-        contextSource.setBase(ldapBase);
-        contextSource.setUserDn(ldapUser);
-        contextSource.setPassword(ldapPassword);
+        contextSource.setUrl(ldapProps.url);
+        contextSource.setBase(ldapProps.base);
+        contextSource.setUserDn(ldapProps.user);
+        contextSource.setPassword(ldapProps.password);
         return contextSource;
     }
 
     @Bean
-    public LdapTemplate ldapTemplate() {
-        return new LdapTemplate(contextSource());        
-    }	
-	
+    public LdapTemplate ldapTemplate(LdapContextSource contextSource) {
+        return new LdapTemplate(contextSource);
+    }
+
 }
