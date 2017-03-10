9200d3865983f8f4c14f668bfbec158871f238fe
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-03-10 11:54:18 +0300

    Delete Security Init
    It conflicts with spring boot security configuration

--- src/main/java/ru/excbt/datafuse/nmk/config/security/SecurityInit.java
+++
@@ -1,7 +0,0 @@
-package ru.excbt.datafuse.nmk.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-
-}
