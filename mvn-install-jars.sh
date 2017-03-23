cae589b00a8497ef68566edbaa6a763498662203
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-03-23 17:18:31 +0300

    using mvnw command in bat & sh

--- mvn-install-jars.sh
+++ mvn-install-jars.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn install:install-file -Dfile=./libs/ru/excbt/jasper/fonts_rus/1.0/fonts_rus-1.0.jar -DgroupId=ru.excbt.jasper -DartifactId=fonts_rus -Dversion=1.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./libs/ru/excbt/jasper/jasperreports-functions/6.3.0/jasperreports-functions-6.3.0.jar -DgroupId=ru.excbt.jasper -DartifactId=jasperreports-functions -Dversion=6.3.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./libs/ru/excbt/jasper/fonts_rus/1.0/fonts_rus-1.0.jar -DgroupId=ru.excbt.jasper -DartifactId=fonts_rus -Dversion=1.0 -Dpackaging=jar
+./mvnw install:install-file -Dfile=./libs/ru/excbt/jasper/jasperreports-functions/6.3.0/jasperreports-functions-6.3.0.jar -DgroupId=ru.excbt.jasper -DartifactId=jasperreports-functions -Dversion=6.3.0 -Dpackaging=jar
