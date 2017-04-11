c1376e6ca6fafbe869721f7a25333c4ebca65754
Alexander Artamonov <a.artamonov@excbt.ru>
2017-04-11 11:40:16 +0400

    Upgrade energy passport interface. Dynamic tables.

--- src/main/webapp/app/scripts/controllers/documents-energo-passport.js
+++ src/main/webapp/app/scripts/controllers/documents-energo-passport.js
@@ -1,5 +1,5 @@
-/*jslint node: true*/
-/*global angular*/
+/*jslint node: true, nomen: true*/
+/*global angular, $*/
 'use strict';
 var app = angular.module('portalNMC');
 app.controller('documentsEnergoPassportCtrl', ['$location', 'mainSvc', 'energoPassportSvc', 'notificationFactory', '$scope', '$routeParams', '$timeout', function ($location, mainSvc, energoPassportSvc, notificationFactory, $scope, $routeParams, $timeout) {
@@ -31,23 +31,64 @@ app.controller('documentsEnergoPassportCtrl', ['$location', 'mainSvc', 'energoPa
     }
     
 //console.log(inputTableDef);
-    function performElementsRecursion(elements, level, headerRows) {
+    
+    function performHeaderElementsRecursion(elements, level, rows) {
         if (angular.isUndefined(elements) || elements === null) {
             return;
         }
         elements.forEach(function (elm) {
-            if (headerRows.length <= level) {
-                headerRows.push([]);
+            if (rows.length <= level) {
+                rows.push([]);
             }
-            headerRows[level].push(angular.copy(elm));
-            performElementsRecursion(elm.elements, level + 1, headerRows);
+            
+            rows[level].push(angular.copy(elm));
+            
+            performHeaderElementsRecursion(elm.elements, level + 1, rows);
+        });
+    }
+    
+    function performVerticalElement(subElements, rows) {
+        subElements.forEach(function (se, ind) {
+            if (rows.length <= ind) {
+                rows.push([]);
+            }
+            rows[ind].push(angular.copy(se));
+        });
+    }
+    
+    function performElementsRecursion(elements, level, rows) {
+        if (angular.isUndefined(elements) || elements === null) {
+            return;
+        }
+        elements.forEach(function (elm) {
+            if (rows.length <= level) {
+                rows.push([]);
+            }
+            if (elm.vertical === true) {
+                performVerticalElement(elm.elements, rows);                
+            } else {
+//console.log(level);
+//console.log(rows);
+                var tmp = angular.copy(elm);
+//if (level === 0) {                
+//    console.log(tmp);                
+//}
+                rows[level].push(tmp);
+                performElementsRecursion(elm.elements, level + 1, rows);
+            }
         });
     }
     
     function prepareTableHeaderRecursion(tablePart) {
         var headerRows = [];
-        performElementsRecursion(tablePart.elements, 0, headerRows);        
+        performHeaderElementsRecursion(tablePart.elements, 0, headerRows);
         tablePart.headerRows = headerRows;
+        return tablePart;
+    }
+    function prepareTableRowRecursion(tablePart) {
+        var preparedRows = [];
+        performElementsRecursion(tablePart.elements, 0, preparedRows);
+        tablePart.tbody = preparedRows;
         return tablePart;
     }
     
@@ -56,26 +97,44 @@ app.controller('documentsEnergoPassportCtrl', ['$location', 'mainSvc', 'energoPa
             tablePart = prepareTableHeaderRecursion(tablePart);
             return true;
         }
+        if (tablePart.partType === "ROW") {
+            tablePart = prepareTableRowRecursion(tablePart);
+            return true;
+        }
 
     }
     
     function preparePassDoc(passDoc) {
         //Prepare headers for inner tables
-        switch (passDoc.viewType) {
-        default:
-            passDoc.parts.forEach(function (passDocPart) {
-                if (passDocPart.partType !== "INNER_TABLE") {
-                    return;
-                }         
-                passDocPart.innerPdTable.parts.forEach(performTablePart);
+//        switch (passDoc.viewType) {
+//        default:
+        passDoc.parts.forEach(function (passDocPart) {
+            if (passDocPart.partType !== "INNER_TABLE") {
+                return;
+            }
+            var theads = [];
+            var tbodies = [];
+            passDocPart.innerPdTable.parts.forEach(performTablePart);
+            passDocPart.innerPdTable.parts.forEach(function (part) {
+                if (part.partType === "HEADER") {
+//                    Array.prototype.push.apply(theads, part.headerRows);
+                    theads = theads.concat(part.headerRows);
+                }
+                if (part.partType === "ROW") {
+//                    Array.prototype.push.apply(tbodies, part.tbody);
+                    tbodies = tbodies.concat(part.tbody);
+                }
             });
-            break;
+            passDocPart.innerPdTable.theads = theads;
+            passDocPart.innerPdTable.tbodies = tbodies;
+        });
+//            break;
 //        case "TABLE":
 //            passDoc.parts.forEach(performTablePart);
 //            break;
-        }
+//        }
         return passDoc;
-    }    
+    }
     
     $scope.passDocStructure = null;
     //$scope.passDocStructureFromServer = null;
@@ -92,7 +151,7 @@ app.controller('documentsEnergoPassportCtrl', ['$location', 'mainSvc', 'energoPa
             result.push(preparePassDoc(JSON.parse(secTempl.sectionJson)));
         });
 //        result = preparePassDoc(result);
-console.log(result);        
+console.log(result);
 //return;        
         $scope.passDocStructure = result;
         if ($scope.passDocStructure.length >= 1) {
@@ -102,7 +161,7 @@ console.log(result);
                 $(':input').inputmask();
 //                    $(':input').focus();
             }, 0);
-        }        
+        }
         $scope.ctrlSettings.passportLoading = false;
     }
     
@@ -111,7 +170,7 @@ console.log(result);
             .then(successCreatePassportCallback, errorCallback);
     }
     
-    $scope.contentsPartSelect = function(part) {
+    $scope.contentsPartSelect = function (part) {
 //console.log(part);
         $scope.currentPassDocPart.isSelected = false;
         $scope.currentPassDocPart = part;
@@ -124,12 +183,12 @@ console.log(result);
     };
     
     $scope.tdBlur = function (cell) {
-console.log(cell);        
+//console.log(cell);
     };
     
     $scope.cancelEnergoPassportEdit = function () {
         $location.path("/documents/energo-passports");
-    }
+    };
     
     $scope.addRowToTable = function (part) {
         var addingRow = angular.copy(part.innerPdTable.parts[1]);
@@ -140,28 +199,28 @@ console.log(cell);
         });
         //if need - clear row values;
         part.innerPdTable.parts.push(addingRow);
-    }
+    };
     
     $scope.deleteRowFromTable = function (part, ind) {
         part.innerPdTable.parts.splice(ind, 1);
         part.innerPdTable.parts.forEach(function (row, rowInd) {
-            if (row.partType === 'HEADER' || angular.isUndefined(row.dynamic) || (row.dynamic !== true)) {
+            if (row.partType === 'HEADER' || angular.isUndefined(row.dynamic) || row.dynamic !== true) {
                 return false;
             }
             row.elements.some(function (rowElem) {
                 if (rowElem.__type === 'Counter') {
                     rowElem.value = rowInd;
                 }
-            })
+            });
         });
-    }
+    };
     
     function initCtrl() {
         var routeParams = $routeParams;
         if (routeParams.param === "new") {
             createPassDocInit();
         } else {
-console.log("Don't understand request!");            
+            console.log("Don't understand request!");
         }
     }
     
