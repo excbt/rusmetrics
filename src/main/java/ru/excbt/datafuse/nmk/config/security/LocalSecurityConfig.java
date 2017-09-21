f2390d1871b1a12398739c8b98ba4c76e9a6d492
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-09-21 10:41:21 +0300

    Swagger-UI feature

--- src/main/java/ru/excbt/datafuse/nmk/config/security/LocalSecurityConfig.java
+++ src/main/java/ru/excbt/datafuse/nmk/config/security/LocalSecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
@@ -42,6 +44,18 @@ public class LocalSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(userAuthenticationProvider);
 	}
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            //.antMatchers("/app/**/*.{js,html}")
+            .antMatchers("/i18n/**")
+            .antMatchers("/content/**")
+            .antMatchers("/swagger-ui/index.html")
+            .antMatchers("/test/**");
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
