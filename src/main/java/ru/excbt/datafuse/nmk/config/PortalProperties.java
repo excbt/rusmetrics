d900f287f81f811b07d7de830da2d2775ce55481
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-09-22 10:50:51 +0300

    Logback To Logstash feature

--- src/main/java/ru/excbt/datafuse/nmk/config/PortalProperties.java
+++ src/main/java/ru/excbt/datafuse/nmk/config/PortalProperties.java
@@ -286,5 +286,83 @@ public class PortalProperties {
         }
     }
 
+    private final Logging logging = new Logging();
+
+    public Logging getLogging() {
+        return logging;
+    }
+
+    public static class Logging {
+
+        private final Logstash logstash = new Logstash();
+
+        public Logstash getLogstash() {
+            return logstash;
+        }
+
+        public static class Logstash {
+
+            private boolean enabled = false;
+
+            private String host = "localhost";
+
+            private int port = 5000;
+
+            private int queueSize = 512;
+
+            public boolean isEnabled() {
+                return enabled;
+            }
+
+            public void setEnabled(boolean enabled) {
+                this.enabled = enabled;
+            }
+
+            public String getHost() {
+                return host;
+            }
+
+            public void setHost(String host) {
+                this.host = host;
+            }
+
+            public int getPort() {
+                return port;
+            }
+
+            public void setPort(int port) {
+                this.port = port;
+            }
+
+            public int getQueueSize() {
+                return queueSize;
+            }
+
+            public void setQueueSize(int queueSize) {
+                this.queueSize = queueSize;
+            }
+        }
+
+        private final SpectatorMetrics spectatorMetrics = new SpectatorMetrics();
+
+        public SpectatorMetrics getSpectatorMetrics() {
+            return spectatorMetrics;
+        }
+
+        public static class SpectatorMetrics {
+
+            private boolean enabled = false;
+
+            public boolean isEnabled() {
+                return enabled;
+            }
+
+            public void setEnabled(boolean enabled) {
+                this.enabled = enabled;
+            }
+        }
+    }
+
+
 
 }
