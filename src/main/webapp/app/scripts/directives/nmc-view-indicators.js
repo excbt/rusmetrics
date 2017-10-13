fd906c41b87e8a27293950030496e7a0f452a52d
Alexander Artamonov <a.artamonov@excbt.ru>
2017-10-13 17:12:37 +0400

    Upgrade directive for hw/heat/cw indicators

--- src/main/webapp/app/scripts/directives/nmc-view-indicators.js
+++ src/main/webapp/app/scripts/directives/nmc-view-indicators.js
@@ -7,52 +7,53 @@ angular.module('portalNMC')
             restrict: 'E',
             replace: true,
             scope: {
-                contObjectId: "=",
-                contObjectName: "=",
-                contZpointId: "=",
-                contZpointName: "=",
-                contZpointType: "=",
-                deviceModelId: "=",
-                deviceNumber: "=",
-                isManual: "="
+                contObjectId: "@",
+                contObjectName: "@",
+                contZpointId: "@",
+                contZpointName: "@",
+                contZpointType: "@",
+                deviceModelId: "@",
+                deviceModelName: "@",
+                deviceNumber: "@",
+                isManual: "@"
             },
             templateUrl: "scripts/directives/templates/nmc-view-indicators.html",
             controller: ['$scope', 'mainSvc', 'notificationFactory', 'objectSvc', 'indicatorSvc', '$cookies', function ($scope, mainSvc, notificationFactory, objectSvc, indicatorSvc, $cookies) {
                 
-                $scope.modelLoadingFlag = false;
-
-                function errorCallback(e) {
-                    $scope.modelLoadingFlag = false;
-                    var errorObj = mainSvc.errorCallbackHandler(e);
-                    notificationFactory.errorInfo(errorObj.caption, errorObj.description);
-                }
-                
-                function successLoadDeviceModelCallback(resp) {
-                    $scope.modelLoadingFlag = false;
-                    var devModel = resp.data;
-                    $scope.isImpulse = devModel.isImpulse || devModel.deviceType === objectSvc.HEAT_DISTRIBUTOR;
-                    $scope.deviceModelName = devModel.modelName;
+//                $scope.modelLoadingFlag = false;
+//
+//                function errorCallback(e) {
+//                    $scope.modelLoadingFlag = false;
+//                    var errorObj = mainSvc.errorCallbackHandler(e);
+//                    notificationFactory.errorInfo(errorObj.caption, errorObj.description);
+//                }
+//                
+//                function successLoadDeviceModelCallback(resp) {
+//                    $scope.modelLoadingFlag = false;
+//                    var devModel = resp.data;
+//                    $scope.isImpulse = devModel.isImpulse || devModel.deviceType === objectSvc.HEAT_DISTRIBUTOR;
+//                    $scope.deviceModelName = devModel.modelName;
                     
-                    indicatorSvc.setContObjectId($scope.contObjectId);
-                    indicatorSvc.setContObjectName($scope.contObjectName);
-                    indicatorSvc.setDeviceModel($scope.deviceModelName);
-                    indicatorSvc.setDeviceSN($scope.deviceNumber);
-                    indicatorSvc.setZpointId($scope.contZpointId);
-                    indicatorSvc.setZpointName($scope.contZpointName);
-                }
+//                    indicatorSvc.setContObjectId($scope.contObjectId);
+//                    indicatorSvc.setContObjectName($scope.contObjectName);
+//                    indicatorSvc.setDeviceModel($scope.deviceModelName);
+//                    indicatorSvc.setDeviceSN($scope.deviceNumber);
+//                    indicatorSvc.setZpointId($scope.contZpointId);
+//                    indicatorSvc.setZpointName($scope.contZpointName);
+//                }
                 
-                function loadDeviceModel(devModelId) {
-                    $scope.modelLoadingFlag = true;
-                    objectSvc.getDeviceModel(devModelId)
-                        .then(successLoadDeviceModelCallback, errorCallback);
-                    
-                }
+//                function loadDeviceModel(devModelId) {
+//                    $scope.modelLoadingFlag = true;
+//                    objectSvc.getDeviceModel(devModelId)
+//                        .then(successLoadDeviceModelCallback, errorCallback);
+//                    
+//                }
                 
                 function initCtrl() {
 //                    console.log($scope.contObjectId);
 //                    console.log($scope.contZpointId);
 //                    console.log($scope.deviceModelId);
-                    loadDeviceModel($scope.deviceModelId);
+//                    loadDeviceModel($scope.deviceModelId);
                 }
                 
                 initCtrl();
