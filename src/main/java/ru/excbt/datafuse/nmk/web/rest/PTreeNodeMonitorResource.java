66d0b778bc8987b30593e14d21e02f31ba5a7571
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-10-17 16:43:25 +0300

    PTreeNodeMonitorResource annotation fix

--- src/main/java/ru/excbt/datafuse/nmk/web/rest/PTreeNodeMonitorResource.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/rest/PTreeNodeMonitorResource.java
@@ -4,10 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.excbt.datafuse.nmk.data.model.ContObject;
 import ru.excbt.datafuse.nmk.data.model.dto.PTreeNodeMonitorDTO;
 import ru.excbt.datafuse.nmk.data.model.ids.PortalUserIds;
@@ -20,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequestMapping(value = "/api/p-tree-node-monitor")
 public class PTreeNodeMonitorResource {
 
