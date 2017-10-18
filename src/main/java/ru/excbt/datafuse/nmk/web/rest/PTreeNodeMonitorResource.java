fd48495e8aef1346e7c5b5dce3444e72b8d7cb82
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-10-18 13:39:56 +0300

    PTreeNodeMonotorResource nodeId restricted feature

--- src/main/java/ru/excbt/datafuse/nmk/web/rest/PTreeNodeMonitorResource.java
+++ src/main/java/ru/excbt/datafuse/nmk/web/rest/PTreeNodeMonitorResource.java
@@ -1,16 +1,18 @@
 package ru.excbt.datafuse.nmk.web.rest;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.excbt.datafuse.nmk.data.model.ContObject;
 import ru.excbt.datafuse.nmk.data.model.dto.PTreeNodeMonitorDTO;
 import ru.excbt.datafuse.nmk.data.model.ids.PortalUserIds;
+import ru.excbt.datafuse.nmk.data.model.support.ContZPointIdPair;
 import ru.excbt.datafuse.nmk.data.service.CurrentSubscriberService;
 import ru.excbt.datafuse.nmk.data.service.ObjectAccessService;
 import ru.excbt.datafuse.nmk.data.service.PTreeNodeMonitorService;
+import ru.excbt.datafuse.nmk.data.service.SubscrObjectTreeContObjectService;
+import ru.excbt.datafuse.nmk.service.utils.ObjectAccessUtil;
 import ru.excbt.datafuse.nmk.web.rest.support.ApiResponse;
 
 import java.util.ArrayList;
@@ -27,26 +29,40 @@ public class PTreeNodeMonitorResource {
 
     private final ObjectAccessService objectAccessService;
 
+    private final SubscrObjectTreeContObjectService subscrObjectTreeContObjectService;
+
     @Autowired
-    public PTreeNodeMonitorResource(CurrentSubscriberService currentSubscriberServicel, PTreeNodeMonitorService pTreeNodeMonitorService, ObjectAccessService objectAccessService) {
+    public PTreeNodeMonitorResource(CurrentSubscriberService currentSubscriberServicel, PTreeNodeMonitorService pTreeNodeMonitorService, ObjectAccessService objectAccessService, SubscrObjectTreeContObjectService subscrObjectTreeContObjectService) {
         this.currentSubscriberServicel = currentSubscriberServicel;
         this.pTreeNodeMonitorService = pTreeNodeMonitorService;
         this.objectAccessService = objectAccessService;
+        this.subscrObjectTreeContObjectService = subscrObjectTreeContObjectService;
     }
 
 
     @GetMapping("/all-linked-objects")
     @ApiOperation("Get all monitor status of cont objects")
-    public ResponseEntity<?> getLinkedObjectsMonitor(@RequestParam(value = "nodeId", required = false) Long nodeId) {
+    public ResponseEntity<?> getLinkedObjectsMonitor(@ApiParam("nodeId of requested objectMonitor") @RequestParam(value = "nodeId", required = false) Long nodeId) {
+
+        if (nodeId == null) {
+            return ApiResponse.responseNoContent();
+        }
 
         PortalUserIds portalUserIds = currentSubscriberServicel.getSubscriberParam().asPortalUserIds();
 
-        List<Long> availableContObjectIds = objectAccessService.findContObjectIds(portalUserIds);
+        ObjectAccessUtil objectAccessUtil = objectAccessService.objectAccessUtil();
+        List<Long> nodeContObjectIds = subscrObjectTreeContObjectService.selectTreeContObjectIdsAllLevels(portalUserIds, nodeId);
+
+        List<ContZPointIdPair> nodeZPoint = objectAccessService.findAllContZPointPairIds(portalUserIds).stream()
+            .filter(i -> nodeContObjectIds.contains(i.getContObjectId())).collect(Collectors.toList());
+
+        List<Long> monitorContObjectIds = nodeContObjectIds.stream()
+            .filter(objectAccessUtil.checkContObjectId(portalUserIds)).collect(Collectors.toList());
 
         List<PTreeNodeMonitorDTO> resultList = new ArrayList<>();
 
-        List<PTreeNodeMonitorDTO> coMonitorDTOList = pTreeNodeMonitorService.findPTreeNodeMonitorCO(portalUserIds, availableContObjectIds, null, false);
-        List<PTreeNodeMonitorDTO> zpMonitorDTOList = pTreeNodeMonitorService.findPTreeNodeMonitorZP(portalUserIds, coMonitorDTOList);
+        List<PTreeNodeMonitorDTO> coMonitorDTOList = pTreeNodeMonitorService.findPTreeNodeMonitorCO(portalUserIds, monitorContObjectIds, null, false);
+        List<PTreeNodeMonitorDTO> zpMonitorDTOList = pTreeNodeMonitorService.findPTreeNodeMonitorZP(portalUserIds, nodeZPoint, coMonitorDTOList);
         resultList.addAll(coMonitorDTOList);
         resultList.addAll(zpMonitorDTOList);
 
