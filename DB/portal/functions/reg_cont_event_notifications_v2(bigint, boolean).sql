f36bd73f4445d26fc6da694449e63f781e0a7742
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-10-24 11:27:25 +0300

    DB. reg_cont_event_notifications_v2

---
+++ DB/portal/functions/reg_cont_event_notifications_v2(bigint, boolean).sql
@@ -0,0 +1,54 @@
+﻿-- Function: portal.reg_cont_event_notifications_v2(bigint, boolean)
+
+-- DROP FUNCTION portal.reg_cont_event_notifications_v2(bigint, boolean);
+
+CREATE OR REPLACE FUNCTION portal.reg_cont_event_notifications_v2(
+    p_cont_event_id bigint,
+    p_ignore_errors boolean)
+  RETURNS void AS
+$BODY$
+DECLARE
+    subscrs RECORD;
+begin
+
+RAISE NOTICE 'Registering notification for cont_event id=% ', p_cont_event_id;
+
+-- Registering notifications
+for subscrs in select ce.id cont_event_id, sco.cont_object_id, sco.subscriber_id, s.subscriber_info
+		from cont_event ce, subscr_cont_object sco, subscriber s, cont_object co
+		where 	ce.id = p_cont_event_id and 
+			ce.cont_object_id = sco.cont_object_id and 
+			sco.subscriber_id = s.id and
+			sco.cont_object_id = co.id and 
+			ce.deleted = 0 and
+			s.deleted = 0 and 
+			co.deleted = 0 AND
+			sco.subscr_end_date is null
+loop
+	--RAISE NOTICE 'Processing subscriber id=% info=%', subscrs.subscriber_id, subscrs.subscriber_info;
+	BEGIN
+		PERFORM portal.reg_subscr_cont_event_notification_v2(subscrs.subscriber_id,subscrs.cont_event_id);
+	EXCEPTION
+		when OTHERS THEN 
+			IF p_ignore_errors = TRUE THEN
+				RAISE NOTICE 'Ingore Error during register cont_event for subscriber_id=%, cont_event_id=%, SQLERRM=%, SQLSTATE=%', subscrs.subscriber_id,subscrs.cont_event_id, SQLERRM, SQLSTATE;
+			ELSE
+				RAISE;
+				--RAISE EXCEPTION  '% %', SQLERRM, SQLSTATE;
+				--RAISE EXCEPTION 'Error during register cont_event for subscriber_id=%, cont_event_id=%', subscrs.subscriber_id,subscrs.cont_event_id;
+			END IF;
+			
+	END;	
+
+end loop;
+
+	
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION portal.reg_cont_event_notifications_v2(bigint, boolean)
+  OWNER TO dbuser1;
+GRANT EXECUTE ON FUNCTION portal.reg_cont_event_notifications_v2(bigint, boolean) TO dbuser1;
+GRANT EXECUTE ON FUNCTION portal.reg_cont_event_notifications_v2(bigint, boolean) TO public;
+GRANT EXECUTE ON FUNCTION portal.reg_cont_event_notifications_v2(bigint, boolean) TO portal_role;
