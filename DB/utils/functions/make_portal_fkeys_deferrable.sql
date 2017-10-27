1dec5bd8351b48dbe5825b85ee4a39109a375b36
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-10-27 16:43:37 +0300

    Make foreign keys deferrable upgrade

---
+++ DB/utils/functions/make_portal_fkeys_deferrable.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION utils.make_portal_fkeys_deferrable() RETURNS void AS $$
+declare
+   tables RECORD;
+   statement TEXT;
+begin
+FOR tables IN 
+	SELECT table_schema, table_name
+	FROM information_schema.tables t INNER JOIN information_schema.schemata s 
+	ON s.schema_name = t.table_schema 
+	WHERE t.table_schema NOT IN ('pg_catalog', 'information_schema')
+	and t.table_schema IN ('portal', 'public')
+	AND t.table_type NOT IN ('VIEW')
+	ORDER BY table_schema || '.' || table_name			
+LOOP
+   statement := 'SELECT utils.make_fkeys_deferrable(''' || tables.table_schema || ''',''' || tables.table_name || ''')';
+   EXECUTE statement;
+END LOOP;
+return;  
+end;
+$$ LANGUAGE plpgsql;
