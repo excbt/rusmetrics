504e9616bc37e058471cdcd0a67a9b250533b082
Alexander Artamonov <a.artamonov@excbt.ru>
2017-11-09 19:32:14 +0300

    Upgrate angular to 1.6.6

--- src/main/webapp/app/bower_components/angular-touch/angular-touch.js
+++ src/main/webapp/app/bower_components/angular-touch/angular-touch.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.11
+ * @license AngularJS v1.6.6
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -28,6 +28,8 @@
 // define ngTouch module
 /* global -ngTouch */
 var ngTouch = angular.module('ngTouch', []);
+
+ngTouch.info({ angularVersion: '1.6.6' });
 
 ngTouch.provider('$touch', $TouchProvider);
 
