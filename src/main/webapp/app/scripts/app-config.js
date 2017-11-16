bc46af1bf727acc4d2c54e02e66c58b4da8bcfda
Alexander Artamonov <a.artamonov@excbt.ru>
2017-11-16 12:44:15 +0300

    Refactor app-config.

--- src/main/webapp/app/scripts/app-config.js
+++ src/main/webapp/app/scripts/app-config.js
@@ -85,7 +85,7 @@
 
 }());
 
-var app = angular.module('tempPortalNMC', []);
+//var app = angular.module('tempPortalNMC', []);
 //configure $log service
 //app.config(['$logProvider', function ($logProvider) {
 //    $logProvider.debugEnabled(true);
@@ -147,148 +147,148 @@ var app = angular.module('tempPortalNMC', []);
 * Define filters (описываем фильтры)
 **********************************************************************************************************
 */
-app.filter('columnFilter', function () {
-    return function (items, props) {
-        var props = [props];
-        var out = [];
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-                var itemMatches = false,
-                    keys = Object.keys(props),
-                    i;
-                for (i = 0; i < keys.length; i += 1) {
-                    var prop = Number(keys[i]);
-                    var text = props[prop].toLowerCase();
-                    if (angular.isDefined(item[text]) && (item[text].toString().toLowerCase().indexOf(text) !== -1)) {
-                        itemMatches = true;
-                        break;
-                    }
-                }
+//app.filter('columnFilter', function () {
+//    return function (items, props) {
+//        var props = [props];
+//        var out = [];
+//        if (angular.isArray(items)) {
+//            items.forEach(function (item) {
+//                var itemMatches = false,
+//                    keys = Object.keys(props),
+//                    i;
+//                for (i = 0; i < keys.length; i += 1) {
+//                    var prop = Number(keys[i]);
+//                    var text = props[prop].toLowerCase();
+//                    if (angular.isDefined(item[text]) && (item[text].toString().toLowerCase().indexOf(text) !== -1)) {
+//                        itemMatches = true;
+//                        break;
+//                    }
+//                }
+//
+//                if (itemMatches) {
+//                    out.push(item);
+//                }
+//            });
+//        } else {
+//      // Let the output be the input untouched
+//            out = items;
+//        }
+//        return out;
+//    };
+//});
 
-                if (itemMatches) {
-                    out.push(item);
-                }
-            });
-        } else {
-      // Let the output be the input untouched
-            out = items;
-        }
-        return out;
-    };
-});
-
-app.filter('propsFilter', function () {
-    return function (items, props) {
-        var out = [];
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-                var itemMatches = false,
-                    keys = Object.keys(props),
-                    i;
-                for (i = 0; i < keys.length; i += 1) {
-                    var prop = keys[i];
-                    var text = props[prop].toLowerCase();
-    //console.log(prop);                        
-    //console.log(item);            
-                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                        itemMatches = true;
-                        break;
-                    }
-                }
-
-                if (itemMatches) {
-                    out.push(item);
-                }
-            });
-        } else {
-      // Let the output be the input untouched
-            out = items;
-        }
-
-        return out;
-    };
-});
+//app.filter('propsFilter', function () {
+//    return function (items, props) {
+//        var out = [];
+//        if (angular.isArray(items)) {
+//            items.forEach(function (item) {
+//                var itemMatches = false,
+//                    keys = Object.keys(props),
+//                    i;
+//                for (i = 0; i < keys.length; i += 1) {
+//                    var prop = keys[i];
+//                    var text = props[prop].toLowerCase();
+//    //console.log(prop);                        
+//    //console.log(item);            
+//                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+//                        itemMatches = true;
+//                        break;
+//                    }
+//                }
+//
+//                if (itemMatches) {
+//                    out.push(item);
+//                }
+//            });
+//        } else {
+//      // Let the output be the input untouched
+//            out = items;
+//        }
+//
+//        return out;
+//    };
+//});
 
 // Filter for zpoint metadata editor
-app.filter('isIntegrators', function () {
-    return function (items, propVal) {
-        var out = [];
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-                if (angular.isUndefined(item.isIntegrator) || item.isIntegrator == null || item.isIntegrator == propVal) {
-                    out.push(item);
-                }
-            });
-        } else {
-      // Let the output be the input untouched
-            out = items;
-        }
+//app.filter('isIntegrators', function () {
+//    return function (items, propVal) {
+//        var out = [];
+//        if (angular.isArray(items)) {
+//            items.forEach(function (item) {
+//                if (angular.isUndefined(item.isIntegrator) || item.isIntegrator == null || item.isIntegrator == propVal) {
+//                    out.push(item);
+//                }
+//            });
+//        } else {
+//      // Let the output be the input untouched
+//            out = items;
+//        }
+//
+//        return out;
+//    };
+//});
 
-        return out;
-    };
-});
+//app.filter('notEmptyCategoriesByContServiceType', ['$filter', function ($filter) {
+//    return function (items, props) {
+////console.log(items);        
+//        var out = [];
+//        if (angular.isArray(items)) {
+//            items.forEach(function (item) {
+//                var filteredReportTypes = $filter('serviceTypesFilter')(item.reportTypes, props);
+//                if (filteredReportTypes.length == 0) {
+//                    return;
+//                }
+//                var checkReportTypes = false;
+//                filteredReportTypes.some(function (rt) {
+//                    if (rt.paramsetsCount && rt.paramsetsCount == rt.checkedParamsetsCount) {
+//                        checkReportTypes = true;
+//                        return true;
+//                    }
+//                });
+//                if (checkReportTypes == true) {
+//                    out.push(item);
+//                }
+//            });
+//        } else {
+//            out = items;
+//        }
+//        return out;
+//    };
+//}]);
 
-app.filter('notEmptyCategoriesByContServiceType', ['$filter', function ($filter) {
-    return function (items, props) {
-//console.log(items);        
-        var out = [];
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-                var filteredReportTypes = $filter('serviceTypesFilter')(item.reportTypes, props);
-                if (filteredReportTypes.length == 0) {
-                    return;
-                }
-                var checkReportTypes = false;
-                filteredReportTypes.some(function (rt) {
-                    if (rt.paramsetsCount && rt.paramsetsCount == rt.checkedParamsetsCount) {
-                        checkReportTypes = true;
-                        return true;
-                    }
-                });
-                if (checkReportTypes == true) {
-                    out.push(item);
-                }
-            });
-        } else {
-            out = items;
-        }
-        return out;
-    };
-}]);
-
-app.filter('notEmptyContServiceTypesByCategory', ['$filter', function ($filter) {
-    return function (items, props) {
-//console.log(items);        
-        var out = [];
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-//console.log(props);                
-//console.log(props.reportTypes);   
-//console.log(item);           
-                var filteredReportTypes = [];
-                if (angular.isDefined(props)) {
-                    filteredReportTypes = $filter('serviceTypesFilter')(props.reportTypes, item);
-                }
-                if (filteredReportTypes.length === 0) {
-                    return;
-                }
-                var checkReportTypes = false;
-                filteredReportTypes.some(function (rt) {
-                    if (rt.paramsetsCount && rt.paramsetsCount === rt.checkedParamsetsCount) {
-                        checkReportTypes = true;
-                        return true;
-                    }
-                });
-                if (checkReportTypes === true) {
-                    out.push(item);
-                }
-            });
-        } else {
-            out = items;
-        }
-        return out;
-    };
-}]);
+//app.filter('notEmptyContServiceTypesByCategory', ['$filter', function ($filter) {
+//    return function (items, props) {
+////console.log(items);        
+//        var out = [];
+//        if (angular.isArray(items)) {
+//            items.forEach(function (item) {
+////console.log(props);                
+////console.log(props.reportTypes);   
+////console.log(item);           
+//                var filteredReportTypes = [];
+//                if (angular.isDefined(props)) {
+//                    filteredReportTypes = $filter('serviceTypesFilter')(props.reportTypes, item);
+//                }
+//                if (filteredReportTypes.length === 0) {
+//                    return;
+//                }
+//                var checkReportTypes = false;
+//                filteredReportTypes.some(function (rt) {
+//                    if (rt.paramsetsCount && rt.paramsetsCount === rt.checkedParamsetsCount) {
+//                        checkReportTypes = true;
+//                        return true;
+//                    }
+//                });
+//                if (checkReportTypes === true) {
+//                    out.push(item);
+//                }
+//            });
+//        } else {
+//            out = items;
+//        }
+//        return out;
+//    };
+//}]);
 
 
 //************************************************************************************
