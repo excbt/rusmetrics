f348e2e81328c892a5cba94da19686860e8d18ce
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2017-11-29 15:18:20 +0300

    ExceptionTranslatorIntTest. Test logging added

--- src/test/java/ru/excbt/datafuse/nmk/web/rest/errors/ExceptionTranslatorIntTest.java
+++ src/test/java/ru/excbt/datafuse/nmk/web/rest/errors/ExceptionTranslatorIntTest.java
@@ -3,13 +3,17 @@ package ru.excbt.datafuse.nmk.web.rest.errors;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.excbt.datafuse.nmk.app.PortalApplication;
+import ru.excbt.datafuse.nmk.web.rest.util.JsonResultViewer;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,6 +28,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PortalApplication.class)
 public class ExceptionTranslatorIntTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ExceptionTranslatorIntTest.class);
 
     @Autowired
     private ExceptionTranslatorTestController controller;
@@ -43,6 +49,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testConcurrencyFailure() throws Exception {
         mockMvc.perform(get("/test/concurrency-failure"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isConflict())
             .andExpect(jsonPath("$.message").value(ErrorConstants.ERR_CONCURRENCY_FAILURE));
     }
@@ -50,6 +58,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testMethodArgumentNotValid() throws Exception {
          mockMvc.perform(post("/test/method-argument").content("{}").contentType(MediaType.APPLICATION_JSON))
+             .andDo(MockMvcResultHandlers.print())
+             .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
              .andExpect(status().isBadRequest())
              .andExpect(jsonPath("$.message").value(ErrorConstants.ERR_VALIDATION))
              .andExpect(jsonPath("$.fieldErrors.[0].objectName").value("testDTO"))
@@ -60,6 +70,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testParameterizedError() throws Exception {
         mockMvc.perform(get("/test/parameterized-error"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.message").value("test parameterized error"))
             .andExpect(jsonPath("$.params.param0").value("param0_value"))
@@ -69,6 +81,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testParameterizedError2() throws Exception {
         mockMvc.perform(get("/test/parameterized-error2"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.message").value("test parameterized error"))
             .andExpect(jsonPath("$.params.foo").value("foo_value"))
@@ -78,6 +92,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testAccessDenied() throws Exception {
         mockMvc.perform(get("/test/access-denied"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isForbidden())
             .andExpect(jsonPath("$.message").value(ErrorConstants.ERR_ACCESS_DENIED))
             .andExpect(jsonPath("$.description").value("test access denied!"));
@@ -86,6 +102,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testMethodNotSupported() throws Exception {
         mockMvc.perform(post("/test/access-denied"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isMethodNotAllowed())
             .andExpect(jsonPath("$.message").value(ErrorConstants.ERR_METHOD_NOT_SUPPORTED))
             .andExpect(jsonPath("$.description").value("Request method 'POST' not supported"));
@@ -94,6 +112,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testExceptionWithResponseStatus() throws Exception {
         mockMvc.perform(get("/test/response-status"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.message").value("error.400"))
             .andExpect(jsonPath("$.description").value("test response status"));
@@ -102,6 +122,8 @@ public class ExceptionTranslatorIntTest {
     @Test
     public void testInternalServerError() throws Exception {
         mockMvc.perform(get("/test/internal-server-error"))
+            .andDo(MockMvcResultHandlers.print())
+            .andDo((r) -> log.info("Result Json:\n {}", JsonResultViewer.objectBeatifyResult(r)))
             .andExpect(status().isInternalServerError())
             .andExpect(jsonPath("$.message").value(ErrorConstants.ERR_INTERNAL_SERVER_ERROR))
             .andExpect(jsonPath("$.description").value("Internal server error"));
