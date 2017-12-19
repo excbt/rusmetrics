3718145c77f5659b4c97497c7734c3fb05fe0d89
Alexander Artamonov <a.artamonov@excbt.ru>
2017-12-19 18:36:20 +0300

    Connect rest for Cont object control component aka widget.

--- src/main/webapp/app/components/object-tree-module/cont-object-control-component/cont-object-control-component.js
+++ src/main/webapp/app/components/object-tree-module/cont-object-control-component/cont-object-control-component.js
@@ -19,6 +19,7 @@
     function contObjectControlComponentController($scope, $element, $attrs, contObjectControlComponentService, $stateParams) {
         /*jshint validthis: true*/
         var ctrl = this;
+        ctrl.objects = [];
         
         var IMG_PATH_MONITOR_TEMPLATE = "components/object-tree-module/cont-object-control-component/object-state-",
             IMG_PATH_MODE_TEMPLATE = "components/object-tree-module/cont-object-control-component/object-mode-winter",
@@ -26,22 +27,89 @@
         
         var contObjectCtrlSvc = contObjectControlComponentService;
         
+        ctrl.columns = [
+            {
+                name: "caption",
+                caption: "Объект",
+                headerClass: "col-xs-3"
+            }, {
+                name: "heat",
+                caption: "СО",
+                headerClass: "col-xs-1",
+                type: "img"
+            }, {
+                name: "hw",
+                caption: "ГВС",
+                headerClass: "col-xs-1",
+                type: "img"
+            }, {
+                name: "cw",
+                caption: "ХВС",
+                headerClass: "col-xs-1",
+                type: "img"
+            }, {
+                name: "el",
+                caption: "Эл-во",
+                headerClass: "col-xs-1",
+                type: "img"
+
+            }
+
+        ];
+        
         function errorCallback(e) {
             console.error(e);
         }
         
+        function successLoadObjectMonitorStateCallback(resp) {
+            if (angular.isUndefined(resp) || resp === null || angular.isUndefined(resp.data) || resp.data === null) {
+                return false;
+            }
+            var inpData = resp.data, //angular.copy(resp.data);
+                tmpObjInfo = {};
+            tmpObjInfo.caption = inpData.contObjectShortInfo.contObjectName || inpData.contObjectShortInfo.contObjectFullName || inpData.contObjectShortInfo.contObjectId;
+            if (angular.isArray(inpData.contZPointMonitorState)) {
+                inpData.contZPointMonitorState.forEach(function (zpoint) {
+                    tmpObjInfo[zpoint.contServiceTypeKeyname] = IMG_PATH_MONITOR_TEMPLATE + zpoint.stateColor.toLowerCase() + IMG_EXT;                    
+                });
+            }
+            ctrl.objects.some(function (obj) {
+                if (inpData.contObjectShortInfo.contObjectId === obj.id) {
+                    for (var k in tmpObjInfo) {
+                        obj[k] = tmpObjInfo[k];
+                    }
+                    obj.loading = false;
+                    return true;
+                }
+            });
+//            ctrl.objects.push(tmpObjInfo);
+            
+        }
+        
         
         function successLoadObjectsCallback(resp) {
-            console.log(resp);
+//            console.log(resp);
             var tmpBuf = angular.copy(resp.data);
-            var testElm = angular.copy(resp.data[0]);
-            testElm.colorKey = "YELLOW";
-            tmpBuf.push(testElm);
+            //test
+//            var testElm = angular.copy(resp.data[0]);
+//            testElm.colorKey = "YELLOW";
+//            tmpBuf.push(testElm);
+            //end test
             tmpBuf.forEach(function (elm) {
-                elm.imgpath = IMG_PATH_MONITOR_TEMPLATE + elm.colorKey.toLowerCase() + IMG_EXT;
-                elm.modepath = IMG_PATH_MODE_TEMPLATE + IMG_EXT;
+                if (elm.nodeType !== "CONT_OBJECT") {
+                    return false;
+                }
+                var obj = {
+                    id: elm.monitorObjectId,
+                    loading: true
+                };
+                ctrl.objects.push(obj);
+                contObjectCtrlSvc.loadContObjectMonitorState(obj.id)
+                    .then(successLoadObjectMonitorStateCallback, errorCallback);
+//                elm.imgpath = IMG_PATH_MONITOR_TEMPLATE + elm.colorKey.toLowerCase() + IMG_EXT;
+//                elm.modepath = IMG_PATH_MODE_TEMPLATE + IMG_EXT;
             });            
-            ctrl.objects = tmpBuf;
+//            ctrl.objects = tmpBuf;
         }
         
         //get objects with statuses
@@ -50,7 +118,7 @@
         };
         
         ctrl.$onInit = function () {
-            console.log($stateParams);
+//            console.log($stateParams);
             var node = $stateParams.node;
             if (angular.isDefined(node) && node !== null) {
                 ctrl.loadObjects(node.id || node._id);
--- src/main/webapp/app/components/object-tree-module/cont-object-control-component/cont-object-control-component.service.js
+++ src/main/webapp/app/components/object-tree-module/cont-object-control-component/cont-object-control-component.service.js
@@ -1,4 +1,7 @@
 /*global angular*/
+/***
+    created by Artamonov A.A. , Dec. 2017
+*/
 (function () {
     'use strict';
 
@@ -11,17 +14,18 @@
     /* @ngInject */
     function Service($http) {
         
-        var API_URL = "../api",
-            P_TREE_NODE_URL = API_URL + "/p-tree-node",
-            P_TREE_NODE_MONITOR_URL = API_URL + "/p-tree-node-monitor/all-linked-objects",            
+        var P_TREE_NODE_URL = "../api/p-tree-node",
+            P_TREE_NODE_MONITOR_URL = "../api/p-tree-node-monitor/all-linked-objects",            
             OBJECT_URL = "../api/subscr/contEvent/notifications/contObject",
             SUBSCR_TREES_URL = '../api/subscr/subscrObjectTree/contObjectTreeType1',
+            CONT_OBJECT_MONITOR_STATE_URL = '../api/widgets/cont-event-monitor/cont-objects/{contObjectId}/monitor-state',
             REFRESH_PERIOD = 600000,
             TREE_ID_FIELD_NAME = "_id",
             NODE_ID_FIELD_NAME = "_id",
             SUBSCR_OBJECT_TREE_CONT_OBJECTS = "SUBSCR_OBJECT_TREE_CONT_OBJECTS";
         
         this.loadNodeObjects = loadNodeObjects;
+        this.loadContObjectMonitorState = loadContObjectMonitorState;
 
         ////////////////
         
@@ -45,6 +49,11 @@
         function loadNodeObjects(nodeId) {
             var url = P_TREE_NODE_MONITOR_URL;
             url = addParamToURL(url, "nodeId", nodeId);
+            return $http.get(url);
+        }
+        
+        function loadContObjectMonitorState(contObjectId) {
+            var url = CONT_OBJECT_MONITOR_STATE_URL.replace('{contObjectId}', contObjectId);
             return $http.get(url);
         }
     }
