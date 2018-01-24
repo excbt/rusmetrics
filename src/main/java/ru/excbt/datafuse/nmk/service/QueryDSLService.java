9d1c21e115c839eba39c0efa172766e1412c007a
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2018-01-24 15:22:21 +0300

    QueryDSLService templates update

--- src/main/java/ru/excbt/datafuse/nmk/service/QueryDSLService.java
+++ src/main/java/ru/excbt/datafuse/nmk/service/QueryDSLService.java
@@ -1,19 +1,28 @@
 package ru.excbt.datafuse.nmk.service;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.PostgreSQLTemplates;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.EntityManagerFactoryInfo;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class QueryDSLService {
 
     private static final Logger log = LoggerFactory.getLogger(QueryDSLService.class);
+
+    public static final SQLTemplates templates = PostgreSQLTemplates.builder().printSchema().build();
 
     @PersistenceContext(unitName = "nmk-p")
     private EntityManager em;
@@ -72,6 +81,6 @@ public class QueryDSLService {
         }
         return result;
 
-    };
+    }
 
 }
