c9207456cddab815d2b1e59a72cbaab19e208a9d
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2018-01-29 15:51:57 +0300

    Application profiles switch database feature

--- src/main/java/ru/excbt/datafuse/nmk/config/PortalProperties.java
+++ src/main/java/ru/excbt/datafuse/nmk/config/PortalProperties.java
@@ -19,6 +19,8 @@ public class PortalProperties {
 
     private final Metrics metrics = new Metrics();
 
+    private final Datasource datasource = new Datasource();
+
     public Async getAsync() {
         return async;
     }
@@ -33,6 +35,10 @@ public class PortalProperties {
 
     public Metrics getMetrics() {
         return metrics;
+    }
+
+    public Datasource getDatasource() {
+        return datasource;
     }
 
     public static class Async {
@@ -362,6 +368,62 @@ public class PortalProperties {
             }
         }
     }
+
+
+    public static class Datasource {
+
+        private String type;
+
+        private String driverClassName;
+
+        private String url;
+
+        private String username;
+
+        private String password;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setDriverClassName(String driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+    }
+
 
 
 
