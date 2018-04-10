353d72ee32344ec6e8fd833c360a50d536cf4950
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2018-04-10 17:34:03 +0300

    NG Form list menu component authorities upgrade

--- src/main/webapp2/app/entities/blocks/form-list-menu.component.ts
+++ src/main/webapp2/app/entities/blocks/form-list-menu.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
 import { MenuItem } from 'primeng/api';
+import { Account,  Principal } from '../../shared';
+import { JhiEventManager } from 'ng-jhipster';
 
 const debounceTimeValue = 150;
 
@@ -15,23 +17,55 @@ export class FormListMenuComponent implements OnInit, AfterViewInit  {
   @Input() headerString: string;
   @ViewChild('input') input: ElementRef;
   @Output() readonly searchAction: EventEmitter<string> = new EventEmitter();
+  @Output() reportAction: EventEmitter<any> = new EventEmitter();
+  @Output() viewAction: EventEmitter<any> = new EventEmitter();
+  @Output() createAction: EventEmitter<any> = new EventEmitter();
 
+  account: Account;
   items: MenuItem[];
 
+  constructor(
+    private principal: Principal,
+    private eventManager: JhiEventManager
+  ) {}
+
   ngOnInit() {
+
+    this.principal.identity().then((account) => {
+      this.account = account;
+      this.updateMenuAvailability(account);
+      console.log('Account authorities: ' + account.authorities);
+    });
+
+    this.registerAuthenticationSuccess();
+
     this.items = [
       {
-        icon: 'far fa-bars fa-2x',
-        styleClass: 'menu1-bars',
+        icon: 'fa-bars',
+        styleClass: 'menu-bars-size',
         items: [{
-          label: 'Отчет'
-
+            icon: 'far fa-download',
+            label: 'Отчет',
+            id: 'report',
+            command: (event) => {
+              this.reportAction.next();
+            }
           },
           {
-            label: 'Свойства'
+            icon: 'fa-eye',
+            label: 'Свойства',
+            id: 'view',
+            command: (event) => {
+              this.viewAction.next();
+            }
           },
           {
-            label: 'Создать'
+            icon: 'fa-plus',
+            label: 'Создать',
+            id: 'create',
+            command: (event) => {
+              this.createAction.next();
+            }
           }
         ]
       }
@@ -46,9 +80,29 @@ export class FormListMenuComponent implements OnInit, AfterViewInit  {
         tap(() => {
                 const val = this.input.nativeElement.value;
                 this.searchAction.emit(val);
-                // this.searchString.next(val);
         })
     ).subscribe();
-}
+  }
+
+  registerAuthenticationSuccess() {
+    this.eventManager.subscribe('authenticationSuccess', (message) => {
+        this.principal.identity().then((account) => {
+            this.account = account;
+            this.updateMenuAvailability(account);
+        });
+    });
+  }
+
+  updateMenuAvailability(account: Account) {
+    const menuLevel1: MenuItem = this.items[0];
+    const menuLevel2: MenuItem[] = <MenuItem[]> menuLevel1.items;
+    const createItem = menuLevel2.find((x) => x.id === 'create');
+    const reportItem = menuLevel2.find((x) => x.id === 'report');
+    const viewItem = menuLevel2.find((x) => x.id === 'view');
+
+    if (createItem) {
+      createItem.visible = account.authorities.filter((x) => x === 'ROLE_SUBSCR_ADMIN').length > 0;
+    }
+  }
 
 }
