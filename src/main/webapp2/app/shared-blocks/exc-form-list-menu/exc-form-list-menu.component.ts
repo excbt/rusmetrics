2cb23c521b9aa0b4853b076648c76381d913bb54
A.Kovtonyuk <a.kovtonyk@excbt.ru>
2018-04-11 14:55:08 +0300

    NG exc-form-menu-component

--- src/main/webapp2/app/shared-blocks/exc-form-list-menu/exc-form-list-menu.component.ts
+++ src/main/webapp2/app/shared-blocks/exc-form-list-menu/exc-form-list-menu.component.ts
@@ -21,6 +21,10 @@ export class ExcFormListMenuComponent implements OnInit, AfterViewInit  {
   @Output() viewAction: EventEmitter<any> = new EventEmitter();
   @Output() createAction: EventEmitter<any> = new EventEmitter();
 
+  @Input() reportEnabled: boolean;
+  @Input() viewEnabled: boolean;
+  @Input() createEnabled: boolean;
+
   account: Account;
   items: MenuItem[];
 
@@ -34,7 +38,6 @@ export class ExcFormListMenuComponent implements OnInit, AfterViewInit  {
     this.principal.identity().then((account) => {
       this.account = account;
       this.updateMenuAvailability(account);
-      console.log('Account authorities: ' + account.authorities);
     });
 
     this.registerAuthenticationSuccess();
@@ -94,15 +97,21 @@ export class ExcFormListMenuComponent implements OnInit, AfterViewInit  {
   }
 
   updateMenuAvailability(account: Account) {
-    const menuLevel1: MenuItem = this.items[0];
-    const menuLevel2: MenuItem[] = <MenuItem[]> menuLevel1.items;
-    const createItem = menuLevel2.find((x) => x.id === 'create');
-    const reportItem = menuLevel2.find((x) => x.id === 'report');
-    const viewItem = menuLevel2.find((x) => x.id === 'view');
-
-    if (createItem) {
-      createItem.visible = account.authorities.filter((x) => x === 'ROLE_SUBSCR_ADMIN').length > 0;
-    }
+    this.updateMenuItemAvailability('create', this.createEnabled, 'ROLE_SUBSCR_ADMIN');
+    this.updateMenuItemAvailability('view', this.viewEnabled);
+    this.updateMenuItemAvailability('report', this.reportEnabled);
   }
 
+  updateMenuItemAvailability(menuId: string,  menuEnabled: boolean, roleName?: string) {
+    const menuLevel1: MenuItem = this.items[0];
+    const menuLevel2: MenuItem[] = <MenuItem[]> menuLevel1.items;
+    const menuItem = menuLevel2.find((x) => x.id === menuId);
+    if (menuItem) {
+      if (roleName) {
+        menuItem.visible = (this.account.authorities.filter((x) => x === roleName).length > 0) && menuEnabled;
+      } else {
+        menuItem.visible = menuEnabled;
+      }
+    }
+  }
 }
