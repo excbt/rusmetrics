a3f96da3589e123e0a35069bbac98bfb6279dedb
Alexander Artamonov <a.artamonov@excbt.ru>
2018-05-11 16:58:44 +0400

    Fix widget switcher button. Tree Node Color Status: Display Node chart.

--- src/main/webapp2/app/components/object-tree/tree-node-color-status/tree-node-color-status.component.ts
+++ src/main/webapp2/app/components/object-tree/tree-node-color-status/tree-node-color-status.component.ts
@@ -1,3 +1,4 @@
+import { Chart } from 'chart.js/dist/Chart.js';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
@@ -8,6 +9,19 @@ import {
     TreeNodeColorStatus
 } from './';
 
+export class Doughnut {
+    data: any[];
+    labels: string[];
+    colors: string[];
+    options: any;
+}
+
+//    this.doughnutTemplate = {};
+//    this.doughnutTemplate.data = [];
+//    this.doughnutTemplate.labels = labels;
+//    this.doughnutTemplate.colors = doughuntColors;
+//    this.doughnutTemplate.options = doughuntOpts;
+
 @Component({
     selector: 'jhi-tree-node-color-status',
     templateUrl: './tree-node-color-status.component.html',
@@ -17,45 +31,81 @@ import {
 })
 export class TreeNodeColorStatusComponent implements OnInit {
 
-    nodeData: any;
+    nodeGraphData: any;
+    nodeGraphOptions: any;
+    doughnuts: any;
+
+    private chartBgColors: string[];
+    private chartLabels: string[];
+    public resources: string[];
+    private statusKeynames: string[];
+
+    private doughuntOpts: any = {
+        responsive: false,
+        cutoutPercentage: 80,
+        legend: {
+            display: true,
+            position: 'right',
+            onClick: this.legendOnClick,
+            labels: {
+                fontSize: 14,
+                generateLabels: this.generateLabels
+            },
+        }/*,
+        legendCallback: legendCallback
+        */
+        /*title: {
+            display: true,
+            postion: "top",
+            text: "<img src = \"components\object-tree-module\cont-object-monitor-component\heat.png\"/> (123)"
+        }*/
+    };
+
+    // settings for resource doughnut grid
+    dougRows = [{val: 1}, {val: 2}];
+    dougCols = [
+        {
+            class: 'ui-g-6',
+            val: 0
+        },
+        {
+            class: 'ui-g-6',
+            val: 1
+        }
+    ];
+
+    doughuntColors = ['#ef473a', '#FDB45C', '#6e7b90'];
+//    this.doughuntColors = doughuntColors;
+    doughnutTemplate: any;
+//    this.doughnutTemplate = {};
+//    this.doughnutTemplate.data = [];
+//    this.doughnutTemplate.labels = labels;
+//    this.doughnutTemplate.colors = doughuntColors;
+//    this.doughnutTemplate.options = doughuntOpts;
 
     constructor(
         private treeNodeColorStatusService: TreeNodeColorStatusService,
          private route: ActivatedRoute
-    ) 
-    {
-//        this.nodeData = {
-//            datasets: [
-//                {
-//                    data: [
-//                        0,
-//                        0,
-//                        0
-//                    ],
-//                    backgroundColor: [
-//                        'red',
-//                        'yellow',
-//                        'green'
-//                    ],
-//                    label: 'Общая статистика по узлу.'
-//                }
-//            ],
-//            labels: [
-//                'Критически',
-//                'Некритические',
-//                'Штатные'
-//            ]
-//        };
-    }
+    ) {}
 
     ngOnInit() {
+        this.chartBgColors = this.treeNodeColorStatusService.getChartBgColors();
+        this.chartLabels = this.treeNodeColorStatusService.getChartLabels();
+        this.resources = this.treeNodeColorStatusService.getResources();
+        this.statusKeynames = this.treeNodeColorStatusService.getStatusKeynames();
+//        for (let res in this.resources) {
+//            this.doughnuts[this.resources[res]] = new Doughnut();
+//        }
+
 console.log(this.route);
 //        this.route.data.subscribe((data) => console.log('Route data: ', data));
         const tmp = this.route.paramMap.pipe(
-            switchMap((params: ParamMap) =>
-                      this.treeNodeColorStatusService
-                        .loadCommonData(params.get('treeNodeId'))
-                     )
+            switchMap((params: ParamMap) => {
+//                for (let res in this.resources) {
+//                    this.initResourceChart(params.get('treeNodeId'), this.resources[res]);
+//                }
+                return this.treeNodeColorStatusService.loadCommonData(params.get('treeNodeId'));
+            })
         );
 
         tmp.subscribe((data) => this.performTreeNodeColorStatus(data));
@@ -63,6 +113,23 @@ console.log(this.route);
 //        this.treeNodeColorStatusService
 //            .loadCommonData()
 //            .subscribe();
+    }
+
+//    performTreeNodeMonitorData(param) {
+//        this.performTreeNodeColorStatus(param);
+//        this.performResourceData(param);
+//    }
+//
+//    performResourceData() {
+//
+//    }
+
+    legendOnClick(ev, label, arg3) {
+        console.log('Label click!');
+        console.log(ev);
+        console.log(label);
+        console.log(label.index);
+        console.log(arg3);
     }
 
     performTreeNodeColorStatus(data: TreeNodeColorStatus[]) {
@@ -74,30 +141,96 @@ console.log('Color data: ', data);
         const yellow = yellowElm ? yellowElm.contObjectCount : 0;
         const green = greenElm ? greenElm.contObjectCount : 0;
 console.log(red, yellow, green);
-        this.nodeData = {
+        this.nodeGraphData = {
             datasets: [
                 {
-                    data: [
-                        red,
-                        yellow,
-                        green
-                    ],
-                    backgroundColor: [
-                        'red',
-                        'yellow',
-                        'green'
-                    ],
+                    data: [ red, yellow, green ],
+                    backgroundColor: this.chartBgColors,
                     label: 'Общая статистика по узлу.'
                 }
             ],
-            labels: [
-                'Критически',
-                'Некритические',
-                'Штатные'
-            ]
+            labels: this.chartLabels
         };
 
-console.log(this.nodeData);
+        this.nodeGraphOptions = {
+            responsive: true,
+            scale: {
+                gridLines: {
+                    color: 'rgba(0, 255, 0, 1)'
+                }
+            },
+            legend: {
+                display: false
+            }
+        };
+
+console.log(this.nodeGraphData);
+console.log(this.nodeGraphOptions);
+    }
+
+    generateLabels(chart) {
+//                            console.log("generateLabels", chart);
+        const data = chart.data;
+//                            console.log(data);
+        if (data.labels.length && data.datasets.length) {
+            const result = data.labels.map(function(label, i) {
+                const meta = chart.getDatasetMeta(0);
+                const ds = data.datasets[0];
+                const arc = meta.data[i];
+                const custom = arc && arc.custom || {};
+                const getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+                const arcOpts = chart.options.elements.arc;
+                const fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
+                const stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
+                const bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
+
+                return {
+                    text: data.datasets[0].data[i]/*label*/,
+                    fillStyle: fill,
+                    strokeStyle: stroke,
+                    lineWidth: bw,
+                    hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
+
+                    // Extra data used for toggling the correct item
+                    index: i
+                };
+            });
+//                                console.log(result);
+            return result;
+        }
+        return [];
+    }
+
+    initResourceChart(nodeId, resource) {
+        this.treeNodeColorStatusService.loadResourceData(nodeId, resource)
+            .subscribe((resp) => {
+// console.log(resp);
+            if (!resp || resp === null) {
+                console.warn('Resource data: ' + resource + ' is empty', resp);
+                return false;
+            }
+            this.doughnuts[resource] = Object.assign({}, this.doughnutTemplate); // angular.copy(this.doughnutTemplate);
+            this.doughnuts[resource].options = Object.assign({}, this.doughnutTemplate.options);
+            this.doughnuts[resource].options.name = resource;
+            this.doughnuts[resource].data = [0, 0, 0];
+            this.doughnuts[resource].allCount = 0;
+            resp.forEach( (colorObj) => this.performResourceColorData(colorObj, resource));
+        });
+    }
+
+    performResourceColorData(colorObj, resource) {
+        this.doughnuts[resource].allCount += colorObj.contObjectCount;
+        switch (colorObj.levelColor) {
+            case 'RED': case 'red':
+                this.doughnuts[resource].data[0] = colorObj.contObjectCount;
+                break;
+            case 'YELLOW': case 'yellow':
+                this.doughnuts[resource].data[1] = colorObj.contObjectCount;
+                break;
+            case 'GREEN': case 'green':
+                this.doughnuts[resource].data[2] = colorObj.contObjectCount;
+                break;
+        }
     }
 
 }
--- src/main/webapp2/app/components/object-tree/tree-node-color-status/tree-node-color-status.service.ts
+++ src/main/webapp2/app/components/object-tree/tree-node-color-status/tree-node-color-status.service.ts
@@ -19,28 +19,49 @@ export class TreeNodeColorStatusService {
     private resourceUrl: string = SERVER_API_URL + 'api/p-tree-node-monitor/node-color-status/{nodeId}';
     private resourceUrlSuffix = '/status-details/{levelColorKeyname}';
 
+    private CHART_BG_COLORS: string[] = ['#ef473a', '#FDB45C', '#6e7b90', '#46BFBD', '#803690', '#337ab7'];
+    private RESOURCES: string[] = ['heat', 'hw', 'cw', 'el'];
+    private CHART_LABELS: string[] = ['Критические', 'Некритические', 'Штатные'];
+    private STATUS_KEYNAMES: string[] = ['RED', 'YELLOW', 'GREEN'];
+
     constructor(
         private http: HttpClient,
 /*         private treeNavigateService: TreeNavigateService */
     ) {}
+
+    getChartBgColors(): string[] {
+        return this.CHART_BG_COLORS;
+    }
+
+    getResources(): string[] {
+        return this.RESOURCES;
+    }
+
+    getChartLabels(): string[] {
+        return this.CHART_LABELS;
+    }
+
+    getStatusKeynames(): string[] {
+        return this.STATUS_KEYNAMES;
+    }
 
     loadCommonData(nodeId?: string): Observable<TreeNodeColorStatus[]> {
         const url = this.resourceUrl.replace(/{nodeId}/, nodeId);
         return this.http.get<TreeNodeColorStatus[]>(url);
     }
 
-//        loadResourceData(nodeId, resourceName) {
-////            var url = RESOURCE_DATA_URL.replace("{resource}", resource);
-//            var url = this.resourceUrl.replace('{nodeId}', nodeId);
+    loadResourceData(nodeId: string, resourceName: string) {
+//            var url = RESOURCE_DATA_URL.replace("{resource}", resource);
+        const url = this.resourceUrl.replace(/{nodeId}/, nodeId);
+//        url += "?contServiceType=" + resourceName;
+        return this.http.get<TreeNodeColorStatus[]>(url, {params: new HttpParams().set('contServiceType', resourceName) });
+    }
+
+    loadNodeColorStatusDetails(nodeId, levelColor, resourceName) {
+        const url = (this.resourceUrl + this.resourceUrlSuffix).replace('{nodeId}', nodeId).replace('{levelColorKeyname}', levelColor);
+//        if (angular.isDefined(resourceName) && resourceName !== null) {
 //            url += "?contServiceType=" + resourceName;
-//            return $http.get(url);
 //        }
-//
-//        loadNodeColorStatusDetails(nodeId, levelColor, resourceName) {
-//            var url = (this.resourceUrl + this.resourceUrlSuffix).replace("{nodeId}", nodeId).replace("{levelColorKeyname}", levelColor);
-//            if (angular.isDefined(resourceName) && resourceName !== null) {
-//                url += "?contServiceType=" + resourceName;
-//            }
-//            return $http.get(url);
-//        }
+        return this.http.get<TreeNodeColorStatus>(url, {params: new HttpParams().set('contServiceType', resourceName)});
+    }
 }
